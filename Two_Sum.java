2cee900060552c717e6068edd04d29c0950d202f
Jintao Guan <jintaoguan@yahoo.com>
2013-03-12 19:32:48 -0400

    first commit

---
+++ Two_Sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+		HashMap m = new HashMap< Integer, Integer >();
+		int i = 0;
+		int j = numbers.length - 1;
+		for( int i = 0; i < numbers.length; ++i )
+		{
+			if( !m.containKey( numbers[i] ) )
+			{
+			}
+		}
+    }
+}
