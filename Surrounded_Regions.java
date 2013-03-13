47b50740ec05addb5abc931fb5d1dcd9f87ed73a
Jintao Guan <jintaoguan@yahoo.com>
2013-03-13 00:59:51 -0400

    surrounded region

---
+++ Surrounded_Regions.java
@@ -0,0 +1,47 @@
+public class Solution {
+	public final int[][] dir = { {1,0},{0,1} ,{-1,0},{0,-1}};
+	public void solve(char[][] board) {
+    	// Start typing your Java solution below
+    	// DO NOT write main() function
+    	int row = board.length;
+    	if( row == 0 ) return;
+    	int col = board[0].length;
+    	for( int i = 0; i < row; ++i )
+    	{
+    		for( int j = 0; j < col; ++j )
+    		{
+    			if( board[i][j] == 'O' )
+    			{
+    				boolean[][] visit = new boolean[row][col];
+    				boolean ok = dfs( board, visit, i, j );
+    				if( ok ) color( board, visit );
+    			}
+    		}
+    	}
+    }
+	public boolean dfs( char[][] board, boolean[][] visit, int x,int y )
+	{
+		if( x < 0 || x >= board.length ) return false;
+		if( y < 0 || y >= board[0].length ) return false;
+		if( board[x][y] == 'X' ) return true;
+		if( visit[x][y] == true ) return true;
+		if( board[x][y] == 'O' )
+		{
+			visit[x][y] = true;
+			for( int i = 0; i < 4; ++i )
+			{
+				int cx = x + dir[i][0];
+				int cy = y + dir[i][1];
+				boolean tmp = dfs( board, visit, cx, cy );
+				if( tmp == false ) return false;
+			}
+		}
+		return true;
+	}
+	public void color( char[][] board, boolean[][] visit )
+	{
+		for( int i = 0; i < board.length; ++i )
+			for( int j = 0; j < board[0].length; ++j )
+				if( visit[i][j] ) board[i][j] = 'X';
+	}
+}
--- Two_Sum.java
+++ Two_Sum.java
@@ -1,17 +1,37 @@
-import java.util.*;
-
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-		HashMap m = new HashMap< Integer, Integer >();
-		int i = 0;
-		int j = numbers.length - 1;
-		for( int i = 0; i < numbers.length; ++i )
-		{
-			if( !m.containKey( numbers[i] ) )
-			{
-			}
-		}
+        int i = 0;
+        int j = numbers.length - 1;
+        int[] x = new int[2];
+        HashMap<Integer, Integer> indexes = new HashMap<Integer, Integer>();
+        for (int k = 0; k < numbers.length; k++) {
+            if( !indexes.containsKey(numbers[k]) )
+                indexes.put(numbers[k], k+1);
+            else if( numbers[k] * 2 == target) {
+                x[0] = indexes.get(numbers[k]);
+                x[1] = k+1;
+                return x;
+            }
+        }
+        Arrays.sort(numbers);
+        while (i < j) {
+            if ( numbers[i] + numbers[j] > target )
+                j--;
+            else if (numbers[i] + numbers[j] < target )
+                i++;
+            else {
+                x[0] = indexes.get(numbers[i]);
+                x[1] = indexes.get(numbers[j]);
+                if (x[0] > x[1]) {
+                    int temp = x[1];
+                    x[1] = x[0];
+                    x[0] = temp;
+                }
+                return x;
+            }
+        }
+        return x;
     }
 }
