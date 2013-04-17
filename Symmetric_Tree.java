d596993f1ab5839056dc79e56defbae437d44ecf
Jintao Guan <jintaoguan@yahoo.com>
2013-04-16 20:07:36 -0400

    solution to Symmetric Tree

--- Symmetric_Tree.java
+++ Symmetric_Tree.java
@@ -1,31 +1,26 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-	public String convert(String s, int nRows) {
+	public boolean isSymmetric(TreeNode root) {
 		// Start typing your Java solution below
 		// DO NOT write main() function
-		if( nRows == 1 ) return s;
-		ArrayList<StringBuffer> row = new ArrayList<StringBuffer>();
-		for( int i = 0; i < nRows; ++i )
-		{
-			StringBuffer sb = new StringBuffer();
-			row.add(sb);
-		}
-		int cur = 0;
-		boolean dir = true;
-		for( int i = 0; i < s.length(); ++i )
-		{
-			row.get(cur).append(s.charAt(i));
-			if( cur == nRows - 1 ) 
-				dir = false;
-			else if( cur == 0 )
-				dir = true;
-			if( dir == true ) 
-				cur++;
-			else if( dir == false ) 
-				cur--;
-		}
-		StringBuffer ans = new StringBuffer();
-		for( int i = 0; i < nRows; ++i )
-			ans.append( row.get(i).toString() );
-		return ans.toString();
+		if( root == null ) return true;
+		return isSymmetric( root.left, root.right );
+	}
+	public boolean isSymmetric( TreeNode left, TreeNode right ) 
+	{
+		if( left == null && right == null ) return true;
+		if( left == null || right == null ) return false;
+		if( left.val != right.val ) return false;
+		boolean bleft = isSymmetric( left.right, right.left );
+		boolean bright = isSymmetric( left.left, right.right );
+		return bleft && bright ;
 	}
 }
