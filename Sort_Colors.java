784d8e8c59ba27278f3667e6363a0b1f7d956d9c
jintaoguan <ilovexiao77@gmail.com>
2013-05-01 02:00:28 -0400

    second solution to Sort Colors

--- Sort_Colors.java
+++ Sort_Colors.java
@@ -1,3 +1,4 @@
+// naive solution
 public class Solution {
 	public void sortColors(int[] A) {
 		// Start typing your Java solution below
@@ -16,6 +17,37 @@ public class Solution {
 			if( map.containsKey(i) )
 				for( int j = 0; j < map.get(i); ++j )
 					A[index++] = i;
+		}
+	}
+}
+
+// one-pass solution
+public class Solution {
+    public void sortColors(int[] A) {
+    	// Start typing your Java solution below
+		// DO NOT write main() function
+		int left = 0;
+		int right = A.length - 1;
+		int cur = 0;
+		while( cur <= right )
+		{
+			if( A[cur] == 0 )
+			{
+				int tmp = A[cur];
+				A[cur] = A[left];
+				A[left] = tmp;
+				left++;
+                cur++;
+			}
+			else if( A[cur] == 2 )
+			{
+				int tmp = A[cur];
+				A[cur] = A[right];
+				A[right] = tmp;
+				right--;
+			}
+            else if( A[cur] == 1 )
+			    cur++;
 		}
 	}
 }
