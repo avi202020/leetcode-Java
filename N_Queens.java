19fe6a447950904d06f65a71eaac16dcefe81cbd
jintaoguan <ilovexiao77@gmail.com>
2013-06-10 10:16:09 -0400

    modify the solution to N-Queens

--- N_Queens.java
+++ N_Queens.java
@@ -4,46 +4,39 @@ public class Solution {
 		// DO NOT write main() function
 		ArrayList<String[]> ans = new ArrayList<String[]>();
 		int[] queen = new int[n];
-		dfs( ans, queen, n, 0 );
+		dfs(ans, queen, 0);
 		return ans;
 	}
 
-	public void dfs( ArrayList<String[]> ans, int[] queen, int maxd, int depth )
-	{
-		if( depth >= maxd )
-		{
+	public void dfs(ArrayList<String[]> ans, int[] queen, int depth) {
+		int maxd = queen.length;
+		if (depth >= maxd) {
 			String[] board = new String[maxd];
-			for( int i = 0; i < maxd; ++i )
-			{
+			for (int i = 0; i < maxd; ++i) {
 				StringBuffer sb = new StringBuffer();
-				for( int j = 0; j < maxd; ++j )
-				{
-					if( queen[i] == j )
+				for (int j = 0; j < maxd; ++j) {
+					if (queen[i] == j)
 						sb.append('Q');
-					else 
+					else
 						sb.append('.');
 				}
 				board[i] = sb.toString();
 			}
 			ans.add(board);
 		}
-		for( int i = 0; i < maxd; ++i )
-		{
-			if( check( queen, depth, i ) )
-			{
+		for (int i = 0; i < maxd; ++i) {
+			if (check(queen, depth, i)) {
 				queen[depth] = i;
-				dfs( ans, queen, maxd, depth + 1 );
+				dfs(ans, queen, depth + 1);
 			}
 		}
 	}
 
-	public boolean check( int[] queen, int x, int y )
-	{
-		for( int i = 0; i < x; ++i )
-		{
-			if( queen[i] == y ) 
+	public boolean check(int[] queen, int x, int y) {
+		for (int i = 0; i < x; ++i) {
+			if (queen[i] == y)
 				return false;
-			else if( Math.abs( queen[i] - y ) == Math.abs( x - i ) )
+			else if (Math.abs(queen[i] - y) == Math.abs(x - i))
 				return false;
 		}
 		return true;
