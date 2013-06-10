14fa1132307e9f5800bec2247c0b82de0735fcfc
jintaoguan <ilovexiao77@gmail.com>
2013-06-10 00:17:28 -0400

    solution to N-Queens II

---
+++ N_Queens_II.java
@@ -0,0 +1,37 @@
+public class Solution {
+	public int count;
+	public int totalNQueens(int n) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		int[] queen = new int[n];
+		this.count = 0;
+		dfs( queen, n, 0 );
+		return this.count;
+	}
+
+	public void dfs( int[] queen, int maxd, int depth )
+	{
+		if( depth >= maxd )
+			this.count++;
+		for( int i = 0; i < maxd; ++i )
+		{
+			if( check( queen, depth, i ) )
+			{
+				queen[depth] = i;
+				dfs( queen, maxd, depth + 1 );
+			}
+		}
+	}
+
+	public boolean check( int[] queen, int x, int y )
+	{
+		for( int i = 0; i < x; ++i )
+		{
+			if( queen[i] == y ) 
+				return false;
+			else if( Math.abs( queen[i] - y ) == Math.abs( x - i ) )
+				return false;
+		}
+		return true;
+	}
+}
