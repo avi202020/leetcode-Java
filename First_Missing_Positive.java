7e8d184739076616c1571c3b0dc2c1d822de1138
jintaoguan <ilovexiao77@gmail.com>
2013-08-30 15:33:35 -0400

    solution to First Missing Positive

---
+++ First_Missing_Positive.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+    	// Start typing your Java solution below
+		// DO NOT write main() function
+		int i = 0;
+		int n = A.length;
+		while (i < n) {
+			if (A[i] != (i + 1) && A[i] >= 1 && A[i] <= n
+					&& A[A[i] - 1] != A[i]){
+				int pos = A[i]-1;
+				int tmp = A[i];
+				A[i] = A[pos];
+				A[pos] = tmp;
+			}
+			else
+				i++;
+		}
+		for (i = 0; i < n; ++i)
+			if (A[i] != (i + 1))
+				return i + 1;
+		return n + 1;
+	}
+}
