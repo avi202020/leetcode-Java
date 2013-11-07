504ed3f3f7ea4bb1155250430296370ea080b5f4
jintaoguan <ilovexiao77@gmail.com>
2013-11-07 17:17:38 -0500

    solution 2 to Copy_List_with_Random_Pointer

--- Copy_List_with_Random_Pointer.java
+++ Copy_List_with_Random_Pointer.java
@@ -6,6 +6,8 @@
  *     RandomListNode(int x) { this.label = x; }
  * };
  */
+
+// Solution No.1   O(n) time, O(n) space
 public class Solution {
 	public RandomListNode copyRandomList(RandomListNode head) {
 		// IMPORTANT: Please reset any member data you declared, as
@@ -32,5 +34,49 @@ public class Solution {
 			p1 = p1.next;
 		}
 		return new_head.next;
+	}
+}
+
+// Solution No.2   O(n) time, O(1) space
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+		// IMPORTANT: Please reset any member data you declared, as
+		// the same Solution instance will be reused for each test case.
+		if (head == null)
+			return null;
+		RandomListNode cur = head;
+		while (cur != null) {
+			RandomListNode node = new RandomListNode(cur.label);
+			RandomListNode next_node = cur.next;
+			cur.next = node;
+			node.next = next_node;
+			cur = next_node;
+		}
+		cur = head;
+		while (cur != null) {
+			RandomListNode next = cur.next;
+			if( cur.random != null )
+				next.random = cur.random.next;
+			cur = next.next;
+		}
+		RandomListNode p1 = head;
+		RandomListNode new_head = head.next;
+		RandomListNode p2 = new_head;
+		cur = new_head.next;
+		int cnt = 0;
+		while( cur != null ) {
+			if( cnt % 2 == 0 )
+			{
+				p1.next = cur;
+				p1 = cur;
+			} else {
+				p2.next = cur;
+				p2 = cur;
+			}
+			cur = cur.next;
+			cnt++;
+		}
+		p1.next = null;
+		return new_head;
 	}
 }
