e8a2bdef9e5542cd10893438c555d5195fc8fa0b
jintaoguan <ilovexiao77@gmail.com>
2013-11-11 17:29:07 -0500

    update solution 1 for Clone Graph

--- Clone_Graph.java
+++ Clone_Graph.java
@@ -36,3 +36,33 @@ public class Solution {
 		}
 	}
 }
+
+// Updated solution 1: dfs
+public class Solution {
+	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+		// Note: The Solution object is instantiated only once and is reused by each test case.
+		if( node == null ) return null;
+		HashMap<UndirectedGraphNode,UndirectedGraphNode> map = new HashMap<UndirectedGraphNode,UndirectedGraphNode>();
+		HashSet<UndirectedGraphNode> visit = new HashSet<UndirectedGraphNode>();
+		UndirectedGraphNode newnode = new UndirectedGraphNode(node.label);
+		map.put(node,newnode);
+		dfs( node, map, visit );
+		return newnode;
+	}
+	public void dfs( UndirectedGraphNode node, HashMap<UndirectedGraphNode,UndirectedGraphNode> map, HashSet<UndirectedGraphNode> visit ) {
+		if( visit.contains(node) )
+			return;
+		visit.add(node);
+		for( int i = 0; i < node.neighbors.size(); ++i ) {
+			UndirectedGraphNode next = node.neighbors.get(i);
+			if( !map.containsKey(next) ) {
+				UndirectedGraphNode newnext = new UndirectedGraphNode(next.label);
+				map.get(node).neighbors.add(newnext);
+				map.put(next, newnext);
+				dfs( next, map, visit );
+			} else {
+				map.get(node).neighbors.add(map.get(next));
+			}
+		}
+	}
+}
