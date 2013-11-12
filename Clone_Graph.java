12a0225d819170b3be492ad821c24d82711ce84a
jintaoguan <ilovexiao77@gmail.com>
2013-11-12 00:19:09 -0500

    solution 2 for Clone Graph

--- Clone_Graph.java
+++ Clone_Graph.java
@@ -66,3 +66,33 @@ public class Solution {
 		}
 	}
 }
+
+// Solution 2: bfs
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if( node == null ) return null;
+        HashMap<UndirectedGraphNode,UndirectedGraphNode> map = new HashMap<UndirectedGraphNode,UndirectedGraphNode>();
+        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        queue.offer(node);
+        UndirectedGraphNode newnode = new UndirectedGraphNode(node.label);
+        map.put(node,newnode);
+        bfs( queue, map );
+        return newnode;
+    }
+    public void bfs( Queue<UndirectedGraphNode> queue, HashMap<UndirectedGraphNode,UndirectedGraphNode> map ) {
+        while( !queue.isEmpty() ) {
+            UndirectedGraphNode node = queue.poll();
+            for( int i = 0; i < node.neighbors.size(); ++i ) {
+                if( !map.containsKey(node.neighbors.get(i)) ) {
+                    UndirectedGraphNode newnode = new UndirectedGraphNode(node.neighbors.get(i).label);
+                    map.put(node.neighbors.get(i), newnode);
+                    map.get(node).neighbors.add(newnode);
+                    queue.offer(node.neighbors.get(i));
+                } else {
+                    map.get(node).neighbors.add(map.get(node.neighbors.get(i)));
+                }
+            }
+        }
+    }
+}
