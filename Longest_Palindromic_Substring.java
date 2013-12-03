b7ff8490b951bee1066f9786acb58f56fdf732ef
jintaoguan <ilovexiao77@gmail.com>
2013-12-03 18:09:50 -0500

    solution 2 using Manacher Algorithm

--- Longest_Palindromic_Substring.java
+++ Longest_Palindromic_Substring.java
@@ -1,3 +1,5 @@
+
+// Solution 1. naive method. time complexity O(n^2) 
 public class Solution {
 	public String longestPalindrome(String s) {
 		// Start typing your Java solution below
@@ -23,4 +25,46 @@ public class Solution {
 		}
 		return str.substring( left + 1, right );
 	}
+}
+
+
+//Solution 2. Manacher algorithm. time complexity O(n)
+public class Solution {
+    public String longestPalindrome(String str) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        int id = 0;
+    	int maxid = 0;
+    	int ans = 1;
+    	int anscenter = 0;
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("$#");
+    	for( int i = 0; i < str.length(); ++i ) {
+    		sb.append(str.charAt(i));
+    		sb.append('#');
+    	}
+    	String s = sb.toString();
+    	int[] p = new int[s.length()];
+    	for( int i = 1; i < s.length(); ++i ) {
+    		if( maxid > i ) {
+    			p[i] = Math.min( p[2*id-i], maxid - i );
+    		}
+    		else {
+    			p[i] = 1;
+    		}
+    		while( i + p[i] < s.length() && s.charAt(i+p[i]) == s.charAt(i-p[i]) )
+    			p[i]++;
+    		if( i + p[i] > maxid ) {
+    			id = i;
+    			maxid = i + p[i];
+    		}
+    		if( p[i] > ans ) { 
+    			ans = p[i];
+    			anscenter = i;
+    		}
+    	}
+    	s = s.substring( anscenter - ans + 1,  anscenter + ans );
+    	s = s.replaceAll("#","");
+    	return s;
+    }
 }
