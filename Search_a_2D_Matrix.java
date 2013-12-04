d13b89f51a355290f5f498385298355ff7eac73b
jintaoguan <ilovexiao77@gmail.com>
2013-12-03 19:54:55 -0500

    solution 2 for Search a 2D Matrix

--- Search_a_2D_Matrix.java
+++ Search_a_2D_Matrix.java
@@ -19,3 +19,31 @@ public class Solution {
 		return false;
 	}
 }
+
+
+
+// Solution 2. totally a Binary Search
+public class Solution {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		// IMPORTANT: Please reset any member data you declared, as
+		// the same Solution instance will be reused for each test case.
+		if( matrix.length == 0 )
+			return false;
+		int m = matrix.length;
+		int n = matrix[0].length;
+		int left = 0;
+		int right = m * n - 1;
+		while( left <= right ) {
+			int mid = ( left + right ) / 2;
+			int row = mid / n;
+			int col = mid % n;
+			if( target > matrix[row][col] )
+				left = mid + 1;
+			else if( target < matrix[row][col] )
+				right = mid - 1;
+			else if( target == matrix[row][col] )
+				return true;
+		}
+		return false;
+	}
+}
