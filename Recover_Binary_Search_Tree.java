c68da86e3e0e412aaa710b2a0dee0e11000e83d2
jintaoguan <ilovexiao77@gmail.com>
2013-12-03 19:38:36 -0500

    solution 2 for Recover Binary Search Tree

--- Recover_Binary_Search_Tree.java
+++ Recover_Binary_Search_Tree.java
@@ -8,8 +8,8 @@
  * }
  */
 public class Solution {
-    public void recoverTree(TreeNode root) {
-    	// Start typing your Java solution below
+	public void recoverTree(TreeNode root) {
+		// Start typing your Java solution below
 		// DO NOT write main() function
 		ArrayList<TreeNode> nodes = new ArrayList<TreeNode>();
 		ArrayList<Integer> values = new ArrayList<Integer>();
@@ -27,5 +27,48 @@ public class Solution {
 		nodes.add( root );
 		values.add( root.val );
 		convert2array( root.right, nodes, values );
+	}
+}
+
+
+// Solution 2. O(1) space complexity
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+	TreeNode pre;
+	TreeNode swap1;
+	TreeNode swap2;
+
+	public void recoverTree(TreeNode root) {
+		if( root == null ) return;
+		ArrayList<TreeNode> swaps = new ArrayList<TreeNode>();
+
+		pre = null;
+		swap1 = null;
+		swap2 = null;
+		dfs( root );
+		int tmp = swap1.val;
+		swap1.val = swap2.val;
+		swap2.val = tmp;
+		return;
+	}
+	public void dfs( TreeNode root ) {
+		if( root == null ) return;
+		dfs( root.left );
+		if( pre != null && root.val < pre.val ) {
+			swap2 = root;
+			if( swap1 == null )
+				swap1 = pre;
+		}
+		pre = root;
+		dfs( root.right);
 	}
 }
