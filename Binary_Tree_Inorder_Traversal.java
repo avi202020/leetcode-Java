5d32b054939cd9e19d61a048cb48e3c1f7c0e664
jintaoguan <ilovexiao77@gmail.com>
2014-02-20 12:09:16 -0500

    update the solution of Morris Traversal

--- Binary_Tree_Inorder_Traversal.java
+++ Binary_Tree_Inorder_Traversal.java
@@ -7,6 +7,9 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
+// Solution 1. Naive and trivial solution using recursion, O(n) time, O(logn) space
 public class Solution {
 	public ArrayList<Integer> inorderTraversal( TreeNode root ) {
 		// Start typing your Java solution below
@@ -22,6 +25,39 @@ public class Solution {
 			inOrderCore( root.left, list );
 			list.add(root.val);
 			inOrderCore( root.right, list );
+		}
+	}
+}
+
+
+// Solution 2. Morris traversal algorithm, O(n) time, O(1) space
+public class Solution {
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		morrisInorder(root, ans);
+		return ans;
+    }
+	public void morrisInorder(TreeNode root, ArrayList<Integer> ans) {
+		TreeNode cur = root;
+		TreeNode prev = null;
+		while (cur != null) {
+			if (cur.left == null) {
+				ans.add(cur.val);
+				cur = cur.right;
+			} else if (cur.left != null) {
+				prev = cur.left;
+				while (prev.right != null && prev.right != cur)
+					prev = prev.right;
+				if (prev.right == null) {
+					prev.right = cur;
+					cur = cur.left;
+				}
+				else if (prev.right == cur) {
+					prev.right = null;
+					ans.add(cur.val);
+					cur = cur.right;
+				}
+			}
 		}
 	}
 }
