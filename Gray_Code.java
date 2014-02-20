ca59a71841ba2c1058eb366250f5bbf16ee32f2f
jintaoguan <ilovexiao77@gmail.com>
2014-02-19 19:35:03 -0500

    solution for Gray Code

---
+++ Gray_Code.java
@@ -0,0 +1,9 @@
+public class Solution {
+	public ArrayList<Integer> grayCode(int n) {
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		int count = 1 << n;
+		for( int i = 0; i < count; ++i )
+			ans.add( i ^ (i >> 1 ));
+		return ans;
+	}
+}
