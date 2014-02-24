be3f3e2075c7ff54c340978596837c028ee942e4
Jintao Guan <ilovexiao77@gmail.com>
2014-02-24 11:34:29 -0500

    update solution of Search in Rotated Sorted Array

--- Search_in_Rotated_Sorted_Array.java
+++ Search_in_Rotated_Sorted_Array.java
@@ -1,25 +1,30 @@
 public class Solution {
-	public int search(int[] A, int target) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int left = 0;
-		int right = A.length - 1;
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			if (A[mid] == target)
-				return mid;
-			if (A[left] <= A[mid]) {
-				if (target >= A[left] && target <= A[mid])
-					right = mid - 1;
-				else
-					left = mid + 1;
-			} else {
-				if (target <= A[right] && target >= A[mid])
-					left = mid + 1;
-				else
-					right = mid - 1;
-			}
-		}
-		return -1;
-	}
+    public int search(int[] A, int target) {
+        int left = 0;
+        int right = A.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (A[mid] == target)
+                return mid;
+            // determine the layout of this array. case 1.
+            else if (A[left] <= A[mid]) {
+                // make sure the target is in the interval [left, mid]
+                if (target >= A[left] && target <= A[mid])
+                    right = mid - 1;
+                // if not, the target is in the other interval (mid, right]
+                else
+                    left = mid + 1;
+            }
+            // determine the layout of this array. case 2.
+            else if (A[left] >= A[mid]) {
+                // make sure the target is in the interval [mid, right]
+                if (A[mid] <= target && target <= A[right])
+                    left = mid + 1;
+                // if not, the target is in the other interval [left, mid)
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
