0b15419a49d2efed1817f4fc58e6d53f2f86477b
Jintao Guan <ilovexiao77@gmail.com>
2014-02-25 15:23:51 -0500

    update solution of Spiral Matrix II

--- Spiral_Matrix_II.java
+++ Spiral_Matrix_II.java
@@ -1,28 +1,29 @@
 public class Solution {
-	int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-	public ArrayList<Integer> spiralOrder(int[][] matrix) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<Integer> ans = new ArrayList<Integer>();
-		if( matrix.length == 0 ) return ans;
-		boolean[][] visit = new boolean[matrix.length][matrix[0].length];
-		dfs( matrix, visit, 0, 0, 0, ans );
-		return ans;
-	}
-	public void dfs( int[][] matrix, boolean[][] visit, int x, int y, int curdir, ArrayList<Integer> ans )
-	{
-		if( ans.size() >= matrix.length * matrix[0].length )
-			return;
-		ans.add( matrix[x][y] );
-		visit[x][y] = true;
-		int nx = x + dir[curdir][0];
-		int ny = y + dir[curdir][1];
-		if( nx >= 0 && nx < matrix.length && ny >= 0 && ny < matrix[0].length && visit[nx][ny] == false )
-			dfs( matrix, visit, nx, ny, curdir, ans );
-		curdir = ( curdir + 1 ) % 4;
-		nx = x + dir[curdir][0];
-		ny = y + dir[curdir][1];
-		dfs( matrix, visit, nx, ny, curdir, ans );
-	}
+    int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        if( n == 0 ) return matrix;
+        boolean[][] visit = new boolean[n][n];
+        dfsGenerateMatrix(0, 0, 1, 0, visit, matrix);
+        return matrix;
+    }
+
+    public void dfsGenerateMatrix(int x, int y, int cur, int curdir, boolean[][] visit, int[][] matrix) {
+        if (cur > visit.length * visit[0].length)
+            return;
+        matrix[x][y] = cur;
+        visit[x][y] = true;
+        int nx = x + dir[curdir][0];
+        int ny = y + dir[curdir][1];
+        if (nx >= 0 && nx < visit.length && ny >= 0 && ny < visit[0].length && visit[nx][ny] == false) {
+            dfsGenerateMatrix(nx, ny, cur + 1, curdir, visit, matrix);
+        } else {
+            curdir = (curdir + 1) % 4;
+            nx = x + dir[curdir][0];
+            ny = y + dir[curdir][1];
+            dfsGenerateMatrix(nx, ny, cur + 1, curdir, visit, matrix);
+        }
+    }
 }
 
