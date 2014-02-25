a4311f2b502f034d6bac394962f710ba80b346fe
Jintao Guan <ilovexiao77@gmail.com>
2014-02-25 15:26:12 -0500

    update solution of Spiral Matrix

--- Spiral_Matrix.java
+++ Spiral_Matrix.java
@@ -1,5 +1,5 @@
 public class Solution {
-    
+
     // four directions
     int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
@@ -21,11 +21,13 @@ public class Solution {
         visit[x][y] = true;
         int nx = x + dir[curdir][0];
         int ny = y + dir[curdir][1];
-        if (nx >= 0 && nx < matrix.length && ny >= 0 && ny < matrix[0].length && visit[nx][ny] == false)
+        if (nx >= 0 && nx < matrix.length && ny >= 0 && ny < matrix[0].length && visit[nx][ny] == false) {
             dfs(matrix, visit, nx, ny, curdir, ans);
-        curdir = (curdir + 1) % 4;
-        nx = x + dir[curdir][0];
-        ny = y + dir[curdir][1];
-        dfs(matrix, visit, nx, ny, curdir, ans);
+        } else {
+            curdir = (curdir + 1) % 4;
+            nx = x + dir[curdir][0];
+            ny = y + dir[curdir][1];
+            dfs(matrix, visit, nx, ny, curdir, ans);
+        }
     }
 }
