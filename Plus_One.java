1c6f31546fb466a46b8e7a3f083838b146fa17fc
Jintao Guan <ilovexiao77@gmail.com>
2014-02-27 16:45:21 -0500

    update solution of Plus One

--- Plus_One.java
+++ Plus_One.java
@@ -1,23 +1,23 @@
 public class Solution {
-	public int[] plusOne(int[] digits) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int len = digits.length;
-		int carry = 1;
-		for( int i = len - 1; i >= 0; --i )
-		{
-			int sum = digits[i] + carry;
-			carry = sum / 10;
-			digits[i] = sum % 10;
-		}
-		if( carry != 0 )
-		{
-			int[] tmp = new int[len+1];
-			for( int i = len; i >= 1; --i )
-				tmp[i] = digits[i-1];
-			tmp[0] = 1;
-			digits = tmp;
-		}
-		return digits;
-	}
+    public int[] plusOne(int[] digits) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int len = digits.length;
+        int carry = 1;
+        for( int i = len - 1; i >= 0; --i )
+        {
+            int sum = digits[i] + carry;
+            carry = sum / 10;
+            digits[i] = sum % 10;
+        }
+        if( carry != 0 )
+        {
+            int[] tmp = new int[len+1];
+            for( int i = len; i >= 1; --i )
+                tmp[i] = digits[i-1];
+            tmp[0] = 1;
+            digits = tmp;
+        }
+        return digits;
+    }
 }
