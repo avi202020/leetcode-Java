761a58e54b313ebf47c128bf4afda13ccc33692e
Jintao Guan <ilovexiao77@gmail.com>
2014-02-27 15:53:57 -0500

    update solution for Balanced Binary Tree

--- Balanced_Binary_Tree.java
+++ Balanced_Binary_Tree.java
@@ -8,29 +8,48 @@
  * }
  */
 
-//**************************************************************
-//                    1.naive method
-//**************************************************************
+// 1.naive method
 public class Solution {
-	public boolean isBalanced(TreeNode root) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( root == null ) 
-			return true;
-		int left = getHeight( root.left );
-		int right = getHeight( root.right );
-		if( Math.abs(left - right) > 1 )
-			return false;
-		boolean bleft = isBalanced( root.left );
-		boolean bright = isBalanced( root.right );
-		return bleft && bright;
-	}
-	public int getHeight( TreeNode root )
-	{
-		if( root == null )
-			return 0;
-		int left = getHeight( root.left );
-		int right = getHeight( root.right );
-		return Math.max( left , right ) + 1;
-	}
+    public boolean isBalanced(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if( root == null ) 
+            return true;
+        int left = getHeight( root.left );
+        int right = getHeight( root.right );
+        if( Math.abs(left - right) > 1 )
+            return false;
+        boolean bleft = isBalanced( root.left );
+        boolean bright = isBalanced( root.right );
+        return bleft && bright;
+    }
+    public int getHeight( TreeNode root )
+    {
+        if( root == null )
+            return 0;
+        int left = getHeight( root.left );
+        int right = getHeight( root.right );
+        return Math.max( left , right ) + 1;
+    }
+}
+
+// 2.better method
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int height = getHeight(root);
+        if( height < 0 ) return false;
+        return true;
+    }
+    public int getHeight( TreeNode root ) {
+        if( root == null )
+            return 0;
+        int lh = getHeight( root.left );
+        if( lh < 0 ) return -1;
+        int rh = getHeight( root.right );
+        if( rh < 0 ) return -1;
+        if( Math.abs(lh-rh) > 1 ) return -1;
+        return Math.max( lh, rh ) + 1;
+    }
 }
