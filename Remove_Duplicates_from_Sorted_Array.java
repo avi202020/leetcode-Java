f3b82c727058ed4e5eaae9ec7ca22cf6566a778e
Jintao Guan <ilovexiao77@gmail.com>
2014-03-04 19:08:27 -0500

    update

--- Remove_Duplicates_from_Sorted_Array.java
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -1,14 +1,14 @@
 public class Solution {
-	public int removeDuplicates(int[] A) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( A.length == 0 ) return 0;
-		int cur = 1;
-		for( int i = 1; i < A.length; ++i  )
-		{
-			if( A[i] != A[i-1] )
-				A[cur++] = A[i];
-		}
-		return cur;
-	}
+    public int removeDuplicates(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if( A.length == 0 ) return 0;
+        int cur = 1;
+        for( int i = 1; i < A.length; ++i  )
+        {
+            if( A[i] != A[i-1] )
+                A[cur++] = A[i];
+        }
+        return cur;
+    }
 }
