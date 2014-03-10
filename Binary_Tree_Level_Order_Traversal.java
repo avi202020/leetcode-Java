a971f77fd1ff51b9d5e094ebf926883bc92b5cf0
Jintao Guan <ilovexiao77@gmail.com>
2014-03-10 14:17:02 -0400

    update solution of Binary Tree Level Order Traverl

--- Binary_Tree_Level_Order_Traversal.java
+++ Binary_Tree_Level_Order_Traversal.java
@@ -12,33 +12,33 @@
 //                  1. iterative method
 //********************************************************
 public class Solution {
-	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-		if( root == null ) return ans;
-		ArrayList<TreeNode> arr = new ArrayList<TreeNode>();
-		arr.add( root );
-		int end = arr.size();
-		int cur = 0;
-		while( cur < arr.size() )
-		{
-			ArrayList<Integer> level = new ArrayList<Integer>();
-			end = arr.size();
-			while( cur < end )
-			{
-				TreeNode node = arr.get(cur);
-				level.add(node.val);
-				if( node.left != null )
-					arr.add( node.left );
-				if( node.right != null )
-					arr.add( node.right );
-				cur++;
-			}
-			ans.add(level);
-		}
-		return ans;
-	}
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        if( root == null ) return ans;
+        ArrayList<TreeNode> arr = new ArrayList<TreeNode>();
+        arr.add( root );
+        int end = arr.size();
+        int cur = 0;
+        while( cur < arr.size() )
+        {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            end = arr.size();
+            while( cur < end )
+            {
+                TreeNode node = arr.get(cur);
+                level.add(node.val);
+                if( node.left != null )
+                    arr.add( node.left );
+                if( node.right != null )
+                    arr.add( node.right );
+                cur++;
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
 }
 
 //********************************************************
@@ -46,27 +46,27 @@ public class Solution {
 //********************************************************
 public class Solution
 {
-	public ArrayList<ArrayList<Integer>> levelOrder( TreeNode root )
-	{
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-		levelOrderCore( root, 0, ans );
-		return ans;
-	}
-	public void levelOrderCore( TreeNode root, int depth, ArrayList<ArrayList<Integer>> ans )
-	{
-		if( root == null ) return;
-		ArrayList<Integer> level = null;
-		if( depth >= ans.size() )
-		{
-			level = new ArrayList<Integer>();
-			ans.add(level);
-		}
-		else 
-			level = ans.get(depth);
-		level.add( root.val );
-		levelOrderCore( root.left, depth + 1, ans );
-		levelOrderCore( root.right, depth + 1, ans );
-	}
+    public ArrayList<ArrayList<Integer>> levelOrder( TreeNode root )
+    {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        levelOrderCore( root, 0, ans );
+        return ans;
+    }
+    public void levelOrderCore( TreeNode root, int depth, ArrayList<ArrayList<Integer>> ans )
+    {
+        if( root == null ) return;
+        ArrayList<Integer> level = null;
+        if( depth >= ans.size() )
+        {
+            level = new ArrayList<Integer>();
+            ans.add(level);
+        }
+        else 
+            level = ans.get(depth);
+        level.add( root.val );
+        levelOrderCore( root.left, depth + 1, ans );
+        levelOrderCore( root.right, depth + 1, ans );
+    }
 }
