a84570f5cd06ee3fac7e995d16d47ca1029957ec
Jintao Guan <ilovexiao77@gmail.com>
2014-03-12 22:00:32 -0400

    update Longest Substring Without Repeating Characters

--- Longest_Substring_Without_Repeating_Characters.java
+++ Longest_Substring_Without_Repeating_Characters.java
@@ -1,30 +1,30 @@
 public class Solution {
-	public int lengthOfLongestSubstring(String s) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( s == null || s.equals("") )
-			return 0;
-		if( s.length() == 1 )
-			return 1;
-		int ans = 0;
-		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-		int start = 0;
-		map.put(s.charAt(start), 0);
-		int cur = 1;
-		while( cur < s.length() ) {
-			if( !map.containsKey(s.charAt(cur)) ) {
-				map.put(s.charAt(cur), cur);
-				if( cur - start + 1 > ans )
-					ans = cur - start + 1;
-				cur++;
-			}
-			else {
-				int pos = map.get(s.charAt(cur));
-				for( int i = start; i <= pos; ++i )
-					map.remove(s.charAt(i));
-				start = pos + 1;
-			}
-		}
-		return ans;
-	}
+    public int lengthOfLongestSubstring(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if( s == null || s.equals("") )
+            return 0;
+        if( s.length() == 1 )
+            return 1;
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int start = 0;
+        map.put(s.charAt(start), 0);
+        int cur = 1;
+        while( cur < s.length() ) {
+            if( !map.containsKey(s.charAt(cur)) ) {
+                map.put(s.charAt(cur), cur);
+                if( cur - start + 1 > ans )
+                    ans = cur - start + 1;
+                cur++;
+            }
+            else {
+                int pos = map.get(s.charAt(cur));
+                for( int i = start; i <= pos; ++i )
+                    map.remove(s.charAt(i));
+                start = pos + 1;
+            }
+        }
+        return ans;
+    }
 }
