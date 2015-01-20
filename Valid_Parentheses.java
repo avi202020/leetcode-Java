282034c7d57f65c8caed2c15f8acd8b6cf2cb985
jintaoguan <ilovexiao77@gmail.com>
2015-01-19 21:43:13 -0800

    modify valid parentheses

--- Valid_Parentheses.java
+++ Valid_Parentheses.java
@@ -1,35 +1,25 @@
 public class Solution {
-	public boolean isValid(String s) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		Stack<Character> stack = new Stack<Character>();
-		for (int i = 0; i < s.length(); ++i) {
-			char cur = s.charAt(i);
-			if (cur == '(')
-				stack.add(cur);
-			else if (cur == '[')
-				stack.add(cur);
-			else if (cur == '{')
-				stack.add(cur);
-			else if (cur == ')') {
-				if (!stack.isEmpty() && stack.peek() == '(')
-					stack.pop();
-				else
-					return false;
-			} else if (cur == ']') {
-				if (!stack.isEmpty() && stack.peek() == '[')
-					stack.pop();
-				else
-					return false;
-			} else if (cur == '}') {
-				if (!stack.isEmpty() && stack.peek() == '{')
-					stack.pop();
-				else
-					return false;
-			}
-		}
-		if (stack.isEmpty())
-			return true;
-		return false;
-	}
+  public boolean isValid(String s) {
+    Stack<Character> stk = new Stack<Character>();
+    for( int i = 0; i < s.length(); ++i ) {
+      char cur = s.charAt(i);
+      if( cur == '(' ||  cur == '[' || cur == '{' ) {
+        stk.push(cur);
+      } else {
+        if( stk.isEmpty() )
+          return false;
+        if( cur == ')' && stk.peek() == '(' )
+          stk.pop();
+        else if ( cur == ']' && stk.peek() == '[' )
+          stk.pop();
+        else if ( cur == '}' && stk.peek() == '{' )
+          stk.pop();
+        else
+          return false;
+      }
+    }
+    if( !stk.isEmpty() )
+      return false;
+    return true;
+  }
 }
