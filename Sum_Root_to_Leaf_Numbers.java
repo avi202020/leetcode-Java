f4a7816a69e2d4331d95e9cb3c7248891b8f29ed
jintaoguan <ilovexiao77@gmail.com>
2015-01-21 19:55:59 -0800

    modify solution for Sum Root to Leaf Numbers

--- Sum_Root_to_Leaf_Numbers.java
+++ Sum_Root_to_Leaf_Numbers.java
@@ -7,23 +7,22 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
 public class Solution {
-	static int sum;
-	public int sumNumbers(TreeNode root) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		sum = 0;
-		dfs( root, 0 );
-		return sum;
-	}
-	public void dfs( TreeNode root, int num )
-	{
-		if( root == null ) return;
-		num = num * 10 + root.val;
-		if( root.left == null && root.right == null )
-			sum += num;
-		dfs( root.left, num );
-		dfs( root.right, num );
-	}
+  private int sum;
+  public int sumNumbers(TreeNode root) {
+    // Start typing your Java solution below
+    // DO NOT write main() function
+    sum = 0;
+    _sumNumbers( root, 0 );
+    return sum;
+  }
+  public void _sumNumbers( TreeNode root, int num )
+  {
+    if( root == null ) return;
+    num = num * 10 + root.val;
+    if( root.left == null && root.right == null )
+      sum += num;
+    _sumNumbers( root.left, num );
+    _sumNumbers( root.right, num );
+  }
 }
