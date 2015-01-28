2539f715d02349e800e2ff0ec4874e11a89ec186
jintaoguan <ilovexiao77@gmail.com>
2015-01-28 12:52:22 -0800

    update solution for Recovery Binary Search Tree

--- Recover_Binary_Search_Tree.java
+++ Recover_Binary_Search_Tree.java
@@ -7,27 +7,28 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// Solution 1. Naive solution using a ArrayList to store every node.
 public class Solution {
-	public void recoverTree(TreeNode root) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<TreeNode> nodes = new ArrayList<TreeNode>();
-		ArrayList<Integer> values = new ArrayList<Integer>();
-		convert2array( root, nodes, values );
-		if( nodes.size() <= 1 ) 
-			return;
-		Collections.sort(values);
-		for( int i = 0; i < nodes.size(); ++i )
-			nodes.get(i).val = values.get(i);
-		return;
-	}
-	public void convert2array( TreeNode root, ArrayList<TreeNode> nodes, ArrayList<Integer> values ) {
-		if( root == null ) return;
-		convert2array( root.left, nodes, values );
-		nodes.add( root );
-		values.add( root.val );
-		convert2array( root.right, nodes, values );
-	}
+  public void recoverTree(TreeNode root) {
+    // Start typing your Java solution below
+    // DO NOT write main() function
+    ArrayList<TreeNode> nodes = new ArrayList<TreeNode>();
+    ArrayList<Integer> values = new ArrayList<Integer>();
+    convert2array( root, nodes, values );
+    if( nodes.size() <= 1 ) 
+      return;
+    Collections.sort(values);
+    for( int i = 0; i < nodes.size(); ++i )
+      nodes.get(i).val = values.get(i);
+    return;
+  }
+  public void convert2array( TreeNode root, ArrayList<TreeNode> nodes, ArrayList<Integer> values ) {
+    if( root == null ) return;
+    convert2array( root.left, nodes, values );
+    nodes.add( root );
+    values.add( root.val );
+    convert2array( root.right, nodes, values );
+  }
 }
 
 
@@ -42,33 +43,32 @@ public class Solution {
  * }
  */
 public class Solution {
-
-	TreeNode pre;
-	TreeNode swap1;
-	TreeNode swap2;
-
-	public void recoverTree(TreeNode root) {
-		if( root == null ) return;
-		ArrayList<TreeNode> swaps = new ArrayList<TreeNode>();
-
-		pre = null;
-		swap1 = null;
-		swap2 = null;
-		dfs( root );
-		int tmp = swap1.val;
-		swap1.val = swap2.val;
-		swap2.val = tmp;
-		return;
-	}
-	public void dfs( TreeNode root ) {
-		if( root == null ) return;
-		dfs( root.left );
-		if( pre != null && root.val < pre.val ) {
-			swap2 = root;
-			if( swap1 == null )
-				swap1 = pre;
-		}
-		pre = root;
-		dfs( root.right);
-	}
+  TreeNode lastNode = null;
+  TreeNode firstNode = null;
+  TreeNode secondNode = null;
+  public void recoverTree(TreeNode root) {
+    if( root == null )
+      return;
+    lastNode = new TreeNode(Integer.MIN_VALUE);
+    firstNode = null;
+    secondNode = null;
+    _recoverTree(root);
+    int tmp = firstNode.val;
+    firstNode.val = secondNode.val;
+    secondNode.val = tmp;
+  }
+  public void _recoverTree(TreeNode root) {
+    if( root == null )
+      return;
+    _recoverTree(root.left);
+    if( firstNode == null && root.val < lastNode.val ) {
+      firstNode = root;
+    }
+    if( firstNode != null && root.val < lastNode.val ) {
+      secondNode = root;
+    }
+    lastNode = root;
+    _recoverTree(root.right);
+  }
 }
+
