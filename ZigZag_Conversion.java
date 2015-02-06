d7ab6c6c53823794460710f23372b0eff3336403
jintaoguan <ilovexiao77@gmail.com>
2015-02-05 21:39:05 -0800

    update solution for ZigZag Conversion

--- ZigZag_Conversion.java
+++ ZigZag_Conversion.java
@@ -1,32 +1,23 @@
 public class Solution {
-	public String convert(String s, int nRows) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-
-		if( nRows == 1 ) return s;
-		ArrayList<StringBuffer> row = new ArrayList<StringBuffer>();
-		for( int i = 0; i < nRows; ++i )
-		{
-			StringBuffer sb = new StringBuffer();
-			row.add(sb);
-		} 
-		int cur = 0;
-		boolean dir = true;
-		for( int i = 0; i < s.length(); ++i )
-		{
-			row.get(cur).append(s.charAt(i));
-			if( cur == nRows - 1 ) 
-				dir = false;
-			else if( cur == 0 )
-				dir = true;
-			if( dir == true ) 
-				cur++;
-			else if( dir == false ) 
-				cur--;
-		}
-		StringBuffer ans = new StringBuffer();
-		for( int i = 0; i < nRows; ++i )
-			ans.append( row.get(i).toString() );
-		return ans.toString();
-	}
+  public String convert(String s, int nRows) {
+    if( nRows == 0 )
+      return "";
+    if( nRows == 1 )
+      return s;
+    ArrayList<StringBuilder> arr = new ArrayList<StringBuilder>();
+    for( int i = 0; i < nRows; ++i )
+      arr.add(new StringBuilder());
+    int dir = 1;
+    int cur = 0;
+    for( int i = 0; i < s.length(); ++i ) {
+      arr.get(cur).append(s.charAt(i));
+      cur = cur + dir;
+      if( cur == 0 || cur == nRows - 1 )
+        dir = dir * (-1);
+    }
+    StringBuilder ans = new StringBuilder();
+    for( int i = 0; i < nRows; ++i )
+      ans.append(arr.get(i).toString());
+    return ans.toString();
+  }
 }
