07d2f050bfd7287cf552d831d4da043675c6349a
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 14:07:05 -0700

    update Generate Parentheses

--- Generate_Parentheses.java
+++ Generate_Parentheses.java
@@ -1,31 +1,26 @@
 public class Solution {
-	public ArrayList<String> generateParenthesis(int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<String> ans = new ArrayList<String>();
-		StringBuffer sb = new StringBuffer();
-		dfs( n, 0, 0, sb, ans );
-		return ans;
-	}
-	public void dfs( int n, int depth, int left_num, StringBuffer sb, ArrayList<String> ans )
-	{
-		if( depth >= 2 * n )
-		{
-			if( left_num == 0 )
-				ans.add( sb.toString() );
-			return;
-		}
-		if( left_num < n )
-		{
-			sb.append('(');
-			dfs( n, depth + 1, left_num + 1, sb, ans );
-			sb.deleteCharAt( sb.length() - 1 );
-		}
-		if( left_num > 0 )
-		{
-			sb.append(')');
-			dfs( n, depth + 1, left_num - 1, sb, ans );
-			sb.deleteCharAt( sb.length() - 1 );
-		}
-	}
+  public List<String> generateParenthesis(int n) {
+    List<String> res = new ArrayList<String>();
+    StringBuilder sb = new StringBuilder();
+    generateParenthesisHelper(n, 0, 0, sb, res);
+    return res;
+  }
+  public void generateParenthesisHelper(int max, int depth, int left, StringBuilder sb, List<String> res) {
+    if (depth >= max * 2) {
+      if (left == 0) {
+        res.add(sb.toString());
+      }
+      return;
+    }
+    if (left < max) {
+      sb.append("(");
+      generateParenthesisHelper(max, depth + 1, left + 1, sb, res);
+      sb.deleteCharAt( sb.length() - 1 );
+    }
+    if (left > 0) {
+      sb.append(")");
+      generateParenthesisHelper(max, depth + 1, left - 1, sb, res);
+      sb.deleteCharAt( sb.length() - 1 );
+    }
+  }
 }
