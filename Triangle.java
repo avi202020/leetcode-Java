2fd6f4f7275721b46bd97a0f7e6f2123e2c14304
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 16:58:19 -0700

    update Triangle

--- Triangle.java
+++ Triangle.java
@@ -1,33 +1,32 @@
 public class Triangle {
-    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if( triangle.size() == 0 ) return 0;
-        if( triangle.size() == 1 ) return triangle.get(0).get(0);
-        int max = Integer.MAX_VALUE;
-        for( int i = 1; i < triangle.size(); ++i )
-        {
-            for( int j = 0; j <= i; ++j )
-            {
-                int now = triangle.get(i).get(j);
-                if( j == 0 ){
-                    int pre = triangle.get(i-1).get(0);
-                    triangle.get(i).set(j, pre + now );
-                }else if( j == i ){
-                    int pre = triangle.get(i-1).get(i-1);
-                    triangle.get(i).set(j, pre + now );
-                }else{
-                    int pre1 = triangle.get(i-1).get(j-1);
-                    int pre2 = triangle.get(i-1).get(j);
-                    int minpre = pre1 < pre2 ? pre1 : pre2;
-                    triangle.get(i).set( j, now + minpre );
-                }
-            }
-        }
-        int index = triangle.size() -1;  
-        for( int i = 0; i < triangle.size(); ++i )
-            if( max > triangle.get(index).get(i) )
-                max = triangle.get(index).get(i);
-        return max;
+  public int minimumTotal(List<List<Integer>> triangle) {
+    if (triangle.size() == 0) {
+      return 0;
     }
+    if (triangle.size() == 1) {
+      return triangle.get(0).get(0);
+    }
+    for (int i = 1; i < triangle.size(); ++i) {
+      for (int j = 0; j <= i; ++j) {
+        int cur = triangle.get(i).get(j);
+        if (j == 0) {
+          int pre = triangle.get(i-1).get(j);
+          triangle.get(i).set(j, cur + pre);
+        } else if (j == i) {
+          int pre = triangle.get(i-1).get(j-1);
+          triangle.get(i).set(j, cur + pre);
+        } else {
+          int pre1 = triangle.get(i-1).get(j-1);
+          int pre2 = triangle.get(i-1).get(j);
+          triangle.get(i).set(j, cur + Math.min(pre1, pre2));
+        }
+      }
+    }
+    int index = triangle.size() - 1;
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < triangle.size(); ++i )
+      if (triangle.get(index).get(i) < min)
+        min = triangle.get(index).get(i);
+    return min;
+  }
 }
