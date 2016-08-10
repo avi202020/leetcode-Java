49d952ed70282ab7b31d1db30637308ea5024a44
jintaoguan <ilovexiao77@gmail.com>
2016-08-10 12:00:37 -0700

    update Unique Binary Search Trees

--- Unique_Binary_Search_Trees.java
+++ Unique_Binary_Search_Trees.java
@@ -1,17 +1,19 @@
 public class Solution {
-	public int numTrees(int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int[] dp = new int[n+1];
-		dp[0] = 1;
-		dp[1] = 1;
-		for( int i = 2; i <= n; ++i )
-		{
-			int sum = 0;
-			for( int j = 0; j < i; ++j )
-				sum += dp[j] * dp[i-1-j];
-			dp[i] = sum;
-		}
-		return dp[n];
-	}
+  public int numTrees(int n) {
+    if (n <= 1) {
+      return 1;
+    }
+    int[] dp = new int[n+1];
+    dp[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      int sum = 0;
+      for (int j = 0; j < i; ++j) {
+        int left = j;
+        int right = i - j - 1;
+        sum = sum + dp[left] * dp[right];
+      }
+      dp[i] = sum;
+    }
+    return dp[n];
+  }
 }
