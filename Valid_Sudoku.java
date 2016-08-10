cc756b5927dd53309a9f46383caf5b473c72be6a
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 17:41:08 -0700

    update Valid Sudoku

--- Valid_Sudoku.java
+++ Valid_Sudoku.java
@@ -1,29 +1,28 @@
 public class Solution {
-	public boolean isValidSudoku(char[][] board) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		boolean[][] row = new boolean[9][10];
-		boolean[][] col = new boolean[9][10];
-		boolean[][] cub = new boolean[9][10];
-		for( int i = 0; i < 9; ++i )
-		{
-			for( int j = 0; j < 9; ++j )
-			{
-				if( board[i][j] == '.' )
-					continue;
-				int num = (int)(board[i][j] - '0');
-				if( false == row[i][num] )
-					row[i][num] = true;
-				else return false;
-				if( false == col[j][num] ) 
-					col[j][num] = true;
-				else return false;
-				int cubindex = i / 3 * 3 + j / 3;
-				if( false == cub[cubindex][num] )
-					cub[cubindex][num] = true;
-				else return false;
-			}
-		}
-		return true;
-	}
+  public boolean isValidSudoku(char[][] board) {
+    boolean[][] row = new boolean[9][10];
+    boolean[][] col = new boolean[9][10];
+    boolean[][] cub = new boolean[9][10];
+    for (int i = 0; i < board.length; ++i) {
+      for (int j = 0; j < board[0].length; ++j) {
+        if (board[i][j] != '.') {
+          int num = board[i][j] - '0';
+          if (row[i][num]) {
+            return false;
+          }
+          if (col[j][num]) {
+            return false;
+          }
+          int cubindex = i / 3 * 3 + j / 3;
+          if (cub[cubindex][num]) {
+            return false;
+          }
+          row[i][num] = true;
+          col[j][num] = true;
+          cub[cubindex][num] = true;
+        }
+      }
+    }
+    return true;
+  }
 }
