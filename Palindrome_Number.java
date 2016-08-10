2cb97aa68040cb93c208aca65ae4c397be4b3d5b
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 17:14:12 -0700

    update Palindrome Number

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -1,43 +1,49 @@
 // 1. naive method 
 public class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0)
-            return false;
-        int div = 1;
-        while (x / div >= 10)
-            div = div * 10;
-        while (x != 0) {
-            int l = x / div;
-            int r = x % 10;
-            if (l != r) 
-                return false;
-            x = (x % div) / 10;
-            div = div / 100;
-        }
-        return true;
+  public boolean isPalindrome(int x) {
+    if (x < 0) {
+      return false;
     }
+    int div = 1;
+    while (x / div >= 10) {
+      div = div * 10;
+    }
+    while (x != 0) {
+      int lh = x / div;
+      int rh = x % 10;
+      if (lh != rh) {
+        return false;
+      }
+      x = (x % div) / 10;
+      div = div / 100;
+    }
+    return true;
+  }
 }
 
 // 2. recursive method 
 public class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0)
-            return false;
-        int div = 1;
-        while (x / div >= 10)
-            div = div * 10;
-        return dfsIsPalindrome(x, div);
+  public boolean isPalindrome(int x) {
+    if (x < 0) {
+      return false;
     }
-
-    public boolean dfsIsPalindrome(int x, int div) {
-        if (x / 10 == 0)
-            return true;
-
-        int l = x / div;
-        int r = x % 10;
-        if (l != r)
-            return false;
-        x = (x % div) / 10;
-        return dfsIsPalindrome(x, div / 100);
+    int div = 1;
+    while (x / div >= 10) {
+      div = div * 10;
     }
+    return dfsIsPalindrome(x, div);
+  }
+
+  public boolean dfsIsPalindrome(int x, int div) {
+    if (x / 10 == 0) {
+      return true;
+    }
+    int l = x / div;
+    int r = x % 10;
+    if (l != r) {
+      return false;
+    }
+    x = (x % div) / 10;
+    return dfsIsPalindrome(x, div / 100);
+  }
 }
