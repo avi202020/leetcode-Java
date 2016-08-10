f4a1c0b8015428d9d5b79a632a1aabde89891b75
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 22:26:11 -0700

    update Simplify Path

--- Simplify_Path.java
+++ Simplify_Path.java
@@ -1,29 +1,30 @@
 public class Solution {
-	public String simplifyPath(String path) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		Stack<String> stack = new Stack<String>();
-		String[] dirs = path.split("/");
-		for (int i = 0; i < dirs.length; ++i) {
-			if (dirs[i].equals(".") || dirs[i].equals(""))
-				continue;
-			else if (dirs[i].equals("..")) {
-				if (!stack.isEmpty())
-					stack.pop();
-			} else
-				stack.add(dirs[i]);
-		}
-		StringBuilder sb = new StringBuilder();
-		ArrayList<String> tmp = new ArrayList<String>();
-		while (!stack.isEmpty()) {
-			tmp.add(stack.pop());
-		}
-		for (int i = tmp.size() - 1; i >= 0; --i) {
-			sb.append("/");
-			sb.append(tmp.get(i));
-		}
-		if (sb.length() == 0)
-			sb.append("/");
-		return sb.toString();
-	}
+  public String simplifyPath(String path) {
+    Stack<String> stack = new Stack<String>();
+    String dirs[] = path.split("/");
+    for (int i = 0; i < dirs.length; ++i) {
+      if (dirs[i].equals(".") || dirs[i].equals("")) {
+        continue;
+      } else if (dirs[i].equals("..")) {
+        if (!stack.isEmpty()) {
+          stack.pop();
+        }
+      } else {
+        stack.push(dirs[i]);
+      }
+    }
+    StringBuilder sb = new StringBuilder();
+    List<String> tmp = new ArrayList<String>();
+    while (!stack.isEmpty()) {
+      tmp.add(stack.pop());
+    }
+    for (int i = tmp.size() - 1; i >=0; i--) {
+      sb.append("/");
+      sb.append(tmp.get(i));
+    }
+    if (sb.length() == 0) {
+      sb.append("/");
+    }
+    return sb.toString();
+  }
 }
