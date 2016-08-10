cdbf5108e6e9042e3fa631565b879c2899aac8ea
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 18:40:58 -0700

    update Binary Tree Level Order Traversal

--- Binary_Tree_Level_Order_Traversal.java
+++ Binary_Tree_Level_Order_Traversal.java
@@ -12,61 +12,56 @@
 //                  1. iterative method
 //********************************************************
 public class Solution {
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-        if( root == null ) return ans;
-        ArrayList<TreeNode> arr = new ArrayList<TreeNode>();
-        arr.add( root );
-        int end = arr.size();
-        int cur = 0;
-        while( cur < arr.size() )
-        {
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            end = arr.size();
-            while( cur < end )
-            {
-                TreeNode node = arr.get(cur);
-                level.add(node.val);
-                if( node.left != null )
-                    arr.add( node.left );
-                if( node.right != null )
-                    arr.add( node.right );
-                cur++;
-            }
-            ans.add(level);
-        }
-        return ans;
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if (root == null) {
+      return res;
     }
+    List<TreeNode> queue = new ArrayList<TreeNode>();
+    queue.add(root);
+    int cur = 0;
+    while (cur < queue.size()) {
+      int end = queue.size();
+      List<Integer> level = new ArrayList<Integer>();
+      while (cur < end) {
+        TreeNode node = queue.get(cur);
+        level.add(node.val);
+        if (node.left != null) {
+          queue.add(node.left);
+        }
+        if (node.right != null) {
+          queue.add(node.right);
+        }
+        cur++;
+      }
+      res.add(level);
+    }
+    return res;
+  }
 }
 
 //********************************************************
 //                  2. recursive method
 //********************************************************
-public class Solution
-{
-    public ArrayList<ArrayList<Integer>> levelOrder( TreeNode root )
-    {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-        levelOrderCore( root, 0, ans );
-        return ans;
+public class Solution {
+  public List<List<Integer>> levelOrder2(TreeNode root) {
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    levelOrderCore(root, 0, ans);
+    return ans;
+  }
+  public void levelOrderCore(TreeNode root, int depth, List<List<Integer>> ans) {
+    if (root == null) {
+      return;
     }
-    public void levelOrderCore( TreeNode root, int depth, ArrayList<ArrayList<Integer>> ans )
-    {
-        if( root == null ) return;
-        ArrayList<Integer> level = null;
-        if( depth >= ans.size() )
-        {
-            level = new ArrayList<Integer>();
-            ans.add(level);
-        }
-        else 
-            level = ans.get(depth);
-        level.add( root.val );
-        levelOrderCore( root.left, depth + 1, ans );
-        levelOrderCore( root.right, depth + 1, ans );
+    List<Integer> level = null;
+    if (depth >= ans.size()) {
+      level = new ArrayList<Integer>();
+      ans.add(level);
+    } else {
+      level = ans.get(depth);
     }
+    level.add(root.val);
+    levelOrderCore(root.left, depth + 1, ans);
+    levelOrderCore(root.right, depth + 1, ans);
+  }
 }
