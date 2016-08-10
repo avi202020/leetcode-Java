a8364668acafeddf443dfee2fbc32765856b415e
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 18:48:43 -0700

    update Count and Say

--- Count_and_Say.java
+++ Count_and_Say.java
@@ -1,33 +1,30 @@
 public class Solution {
-	public String countAndSay(int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( n == 1 ) return "1";
-		StringBuffer sb = new StringBuffer();
-		sb.append("1");
-		char pre;
-		int num;
-		for( int test = 2; test <= n; ++test )
-		{
-			StringBuffer tmp = new StringBuffer();
-			pre = sb.charAt(0);
-			num = 1;
-			for( int i = 1; i < sb.length(); ++i )
-			{
-				if( sb.charAt(i) == pre )
-					num++;
-				else
-				{
-					tmp.append((char)(num+'0'));
-					tmp.append( pre );
-					num = 1;
-					pre = sb.charAt(i);
-				}
-			}
-			tmp.append((char)(num+'0'));
-			tmp.append(pre);
-			sb = tmp;
-		}
-		return sb.toString();
-	}
+  public String countAndSay(int n) {
+    if (n == 1) {
+      return "1";
+    }
+    StringBuilder sb = new StringBuilder();
+    sb.append("1");
+    char pre;
+    int num;
+    for (int i = 2; i <= n; ++i) {
+      StringBuilder newStr = new StringBuilder();
+      pre = sb.charAt(0);
+      num = 1;
+      for (int j = 1; j < sb.length(); ++j) {
+        if (sb.charAt(j) == pre) {
+          num++;
+        } else {
+          newStr.append((char)(num + '0'));
+          newStr.append(pre);
+          pre = sb.charAt(j);
+          num = 1;
+        }
+      }
+      newStr.append((char)(num + '0'));
+      newStr.append(pre);
+      sb = newStr;
+    }
+    return sb.toString();
+  }
 }
