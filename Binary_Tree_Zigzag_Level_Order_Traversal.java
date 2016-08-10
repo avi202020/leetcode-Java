1ddf99c8190a4b2f8b04cb77ca47efc87c70a979
jintaoguan <ilovexiao77@gmail.com>
2016-08-10 16:19:21 -0700

    update Binary Tree Zigzag Level Order Traversal

--- Binary_Tree_Zigzag_Level_Order_Traversal.java
+++ Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -12,38 +12,38 @@
 //                             1. iterative method
 //********************************************************************************
 public class Solution {
-	public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-		if( root == null ) return ans;
-		ArrayList<TreeNode> arr = new ArrayList<TreeNode>();
-		arr.add( root );
-		int end = 0;
-		int cur = 0;
-		int loop = 0;
-		while( cur < arr.size() )
-		{
-			ArrayList<Integer> level = new ArrayList<Integer>();
-			end = arr.size();
-			while( cur < end )
-			{
-				TreeNode node = arr.get(cur);
-				if( loop % 2 == 0 )
-					level.add(node.val);
-				else 
-					level.add( 0, node.val );
-				if( node.left != null )
-					arr.add( node.left );
-				if( node.right != null )
-					arr.add( node.right );
-				cur++;
-			}
-			loop++;
-			ans.add(level);
-		}
-		return ans;
-	}
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if (root == null) {
+      return res;
+    }
+    List<TreeNode> queue = new ArrayList<TreeNode>();
+    queue.add(root);
+    int cur = 0;
+    int levelIndex = 0;
+    while (cur < queue.size()) {
+      int end = queue.size();
+      List<Integer> level = new ArrayList<>();
+      while (cur < end) {
+        TreeNode node = queue.get(cur);
+        if (levelIndex % 2 == 0) {
+          level.add(node.val);
+        } else {
+          level.add(0, node.val);
+        }
+        if (node.left != null) {
+          queue.add(node.left);
+        }
+        if (node.right != null) {
+          queue.add(node.right);
+        }
+        cur++;
+      }
+      res.add(level);
+      levelIndex++;
+    }
+    return res;
+  }
 }
 
 
@@ -51,36 +51,25 @@ public class Solution {
 //                            2. recursive method
 //********************************************************************************
 public class Solution {
-	public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-		int height = getHeight( root );
-		for( int i = 0; i < height; ++i )
-		{
-			ArrayList<Integer> tmp = new ArrayList<Integer>();
-			ans.add(tmp);
-		}
-		levelOrderCore( root, 0, ans );
-		return ans;
-	}
-	public void levelOrderCore( TreeNode root, int depth, ArrayList<ArrayList<Integer>> ans )
-	{
-		if( root == null ) return;
-		ArrayList<Integer> level = ans.get( depth );
-		if( depth % 2 == 0 )
-			level.add( root.val );
-		else
-			level.add( 0, root.val );
-		levelOrderCore( root.left, depth + 1, ans );
-		levelOrderCore( root.right, depth + 1, ans );
-	}
-	public int getHeight( TreeNode root )
-	{
-		if( root == null )
-			return 0;
-		int left = getHeight( root.left );
-		int right = getHeight( root.right );
-		return Math.max( left , right ) + 1;
-	}
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    zigzagLevelOrderHelper(root, 0, ans);
+    return ans;
+  }
+  public void zigzagLevelOrderHelper(TreeNode root, int depth, List<List<Integer>> ans) {
+    if (root == null) {
+      return;
+    }
+    if (depth >= ans.size()) {
+      ans.add(new ArrayList<Integer>());
+    }
+    List<Integer> level = ans.get(depth);
+    if (depth % 2 == 0) {
+      level.add(root.val);
+    } else {
+      level.add(0, root.val);
+    }
+    zigzagLevelOrderHelper(root.left, depth + 1, ans);
+    zigzagLevelOrderHelper(root.right, depth + 1, ans);
+  }
 }
