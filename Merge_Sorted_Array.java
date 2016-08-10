867c3214dcea381f08b375917d4d3f3937b4c69f
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 21:37:08 -0700

    update Merge Sorted Array

--- Merge_Sorted_Array.java
+++ Merge_Sorted_Array.java
@@ -1,13 +1,22 @@
 public class Solution {
-	public void merge(int A[], int m, int B[], int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int indexA = m - 1;
-		int indexB = n - 1;
-		int index = m + n - 1;
-		while( indexA >= 0 && indexB >= 0 )
-			A[index--] = ( A[indexA] >= B[indexB] ? A[indexA--] : B[indexB--] );
-		for( int i = 0; i <= indexB; ++i )
-			A[i] = B[i];
-	}
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = m - 1;
+    int p2 = n - 1;
+    int cur = m + n - 1;
+    while (p1 >= 0 && p2 >= 0) {
+      if (nums1[p1] > nums2[p2]) {
+        nums1[cur] = nums1[p1];
+        p1--;
+      } else {
+        nums1[cur] = nums2[p2];
+        p2--;
+      }
+      cur--;
+    }
+    while (p2 >= 0) {
+      nums1[cur] = nums2[p2];
+      p2--;
+      cur--;
+    }
+  }
 }
