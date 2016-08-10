4f64664b3e92c4af56aee77c9f7da60e7c7269ae
jintaoguan <ilovexiao77@gmail.com>
2016-08-09 21:47:07 -0700

    update Unique Paths

--- Unique_Paths.java
+++ Unique_Paths.java
@@ -1,18 +1,21 @@
 public class Solution {
-	public int uniquePaths(int m, int n) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		int[][] dp = new int[m][n];
-		for( int i = 0; i < m; ++i )
-		{
-			for( int j = 0; j < n; ++j )
-			{
-				if( 0 == i || 0 == j )
-					dp[i][j] = 1;
-				else
-					dp[i][j] = dp[i-1][j] + dp[i][j-1];
-			}
-		}
-		return dp[m-1][n-1];
-	}
+  public int uniquePaths(int m, int n) {
+    int[][] dp = new int[m+1][n+1];
+    for (int i = 0; i <= m; ++i) {
+      dp[i][0] = 0;
+    }
+    for (int i = 0; i <= n; ++i) {
+      dp[0][i] = 0;
+    }
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <=n; ++j) {
+        if (i == 1 || j == 1) {
+          dp[i][j] = 1;
+        } else {
+          dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+      }
+    }
+    return dp[m][n];
+  }
 }
