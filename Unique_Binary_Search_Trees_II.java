1304faeece261e1c6d2902e67c1cca80566d9743
jintaoguan <ilovexiao77@gmail.com>
2016-08-10 17:55:45 -0700

    update Unique Binary Search Trees II

--- Unique_Binary_Search_Trees_II.java
+++ Unique_Binary_Search_Trees_II.java
@@ -8,31 +8,31 @@
  * }
  */
 public class Solution {
-	public ArrayList<TreeNode> generateTrees(int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<TreeNode> ans = _generateTrees( 1, n );
-		return ans;
-	}
-
-	public ArrayList<TreeNode> _generateTrees( int start, int end ) {
-		ArrayList<TreeNode> ans = new ArrayList<TreeNode>();
-		if( start > end ) {
-			ans.add(null);
-			return ans;
-		}
-		for( int i = start; i <= end; ++i ) {
-			ArrayList<TreeNode> left = _generateTrees( start, i-1 );
-			ArrayList<TreeNode> right = _generateTrees( i+1, end );
-			for( int j = 0; j < left.size(); ++j ) {
-				for( int k = 0; k < right.size(); ++k ) {
-					TreeNode node = new TreeNode(i);
-					node.left = left.get(j);
-					node.right = right.get(k);
-					ans.add(node);
-				}
-			}
-		}
-		return ans;
-	}
+  public List<TreeNode> generateTrees(int n) {
+    if (n == 0) {
+      return new ArrayList<TreeNode>();
+    }
+    List<TreeNode> ans = generateTreesHelper(1, n);
+    return ans;
+  }
+  public List<TreeNode> generateTreesHelper(int start, int end) {
+    List<TreeNode> ans = new ArrayList<TreeNode>();
+    if (start > end) {
+      ans.add(null);
+      return ans;
+    }
+    for (int i = start; i <= end; ++i) {
+      List<TreeNode> left = generateTreesHelper(start, i - 1);
+      List<TreeNode> right = generateTreesHelper(i + 1, end);
+      for (int j = 0; j < left.size(); ++j) {
+        for (int k = 0; k < right.size(); ++k) {
+          TreeNode node = new TreeNode(i);
+          node.left = left.get(j);
+          node.right = right.get(k);
+          ans.add(node);
+        }
+      }
+    }
+    return ans;
+  }
 }
