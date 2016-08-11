19c978208d8c72fbbe8b01020bbd7a825a8e9329
jintaoguan <ilovexiao77@gmail.com>
2016-08-11 12:44:10 -0700

    update Binary Search Tree Iterator

--- Binary_Search_Tree_Iterator.java
+++ Binary_Search_Tree_Iterator.java
@@ -8,11 +8,12 @@
  * }
  */
 
+// Solution 1.
 public class BSTIterator {
   Stack<TreeNode> stack;
   public BSTIterator(TreeNode root) {
     stack = new Stack<TreeNode>();
-    while( root != null ) {
+    while (root != null) {
       stack.push(root);
       root = root.left;
     }
@@ -27,14 +28,45 @@ public class BSTIterator {
   public int next() {
     TreeNode node = stack.pop();
     int ret = node.val;
-    if( node.right != null ) {
+    if (node.right != null) {
       node = node.right;
-      while( node != null ) {
+      while (node != null) {
         stack.push(node);
         node = node.left;
       }
     }
     return ret;
+  }
+}
+
+// Solution 2.
+public class BSTIterator {
+  private List<TreeNode> nodes;
+  private int cur;
+  public BSTIterator(TreeNode root) {
+    nodes = new ArrayList<TreeNode>();
+    inorderTraversal(root, nodes);
+    cur = 0;
+  }
+
+  /** @return whether we have a next smallest number */
+  public boolean hasNext() {
+    return cur < nodes.size();
+  }
+
+  /** @return the next smallest number */
+  public int next() {
+    int res = nodes.get(cur).val;
+    cur++;
+    return res;
+  }
+
+  private void inorderTraversal(TreeNode root, List<TreeNode> inorder) {
+    if (root != null) {
+      inorderTraversal(root.left, inorder);
+      inorder.add(root);
+      inorderTraversal(root.right, inorder);
+    }
   }
 }
 
