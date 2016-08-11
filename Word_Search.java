f3904cc911f185036bcfa6011b49423da7f5b4f5
jintaoguan <ilovexiao77@gmail.com>
2016-08-10 18:55:49 -0700

    update Word Search

--- Word_Search.java
+++ Word_Search.java
@@ -1,45 +1,43 @@
 public class Solution {
-    public boolean exist(char[][] board, String word) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int m = board.length;
-        if( m == 0 ) return false;
-        int n = board[0].length;
-        int[][] dir = new int[][]{ { -1, 0 },{ 1, 0 },{ 0, 1 }, { 0, -1 } };
-        for( int i = 0; i < m; ++i )
-        {
-            for( int j = 0; j < n; ++j )
-            {
-                if( board[i][j] == word.charAt(0) )
-                {
-                    boolean found = false;
-                    boolean[][] visit = new boolean[m][n];
-                    found = dfs( board, visit, word, dir, i, j, 0 );
-                    if( found == true ) return true;
-                }
-            }
+  public boolean exist(char[][] board, String word) {
+    int m = board.length;
+    int n = board[0].length;
+    int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (word.charAt(0) == board[i][j]) {
+          boolean[][] visited = new boolean[m][n];
+          boolean success = existHelper(board, word, visited, dir, i, j, 0);
+          if (success) {
+            return true;
+          }
         }
-        return false;
+      }
     }
-    public boolean dfs( char[][] board, boolean[][] visit, String word, int[][] dir, int x, int y, int depth )
-    {
-        if( board[x][y] == word.charAt(depth) )
-        {
-            if( depth == word.length() - 1 ) 
-                return true;
-            visit[x][y] = true;
-            for( int i = 0; i < 4; ++i )
-            {
-                int cx = x + dir[i][0];
-                int cy = y + dir[i][1];
-                if( cx < 0 || cx >= board.length ) continue;
-                if( cy < 0 || cy >= board[0].length ) continue;
-                if( true == visit[cx][cy] ) continue;
-                boolean flag = dfs( board, visit, word, dir, cx, cy, depth + 1 );
-                if( true == flag ) return true;
-            }
-            visit[x][y] = false;
+    return false;
+  }
+  public boolean existHelper(char[][] board, String word, boolean[][] visited, int[][] dirs, int x, int y, int depth) {
+    if (board[x][y] == word.charAt(depth)) {
+      if (depth == word.length() - 1) {
+        return true;
+      }
+      visited[x][y] = true;
+      for (int i = 0; i < 4; ++i) {
+        int nx = x + dirs[i][0];
+        int ny = y + dirs[i][1];
+        if (nx < 0 || nx >= board.length || ny < 0 || ny >= board[0].length) {
+          continue;
         }
-        return false;
+        if (visited[nx][ny]) {
+          continue;
+        }
+        boolean success = existHelper(board, word, visited, dirs, nx, ny, depth + 1);
+        if (success) {
+          return true;
+        }
+      }
+      visited[x][y] = false;
     }
+    return false;
+  }
 }
