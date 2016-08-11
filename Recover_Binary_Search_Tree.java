2a8978d36f3555d3306e8a6c8b5dee3952359578
jintaoguan <ilovexiao77@gmail.com>
2016-08-10 17:34:55 -0700

    update Recover Binary Search Tree

--- Recover_Binary_Search_Tree.java
+++ Recover_Binary_Search_Tree.java
@@ -10,24 +10,25 @@
 // Solution 1. Naive solution using a ArrayList to store every node.
 public class Solution {
   public void recoverTree(TreeNode root) {
-    // Start typing your Java solution below
-    // DO NOT write main() function
-    ArrayList<TreeNode> nodes = new ArrayList<TreeNode>();
-    ArrayList<Integer> values = new ArrayList<Integer>();
-    convert2array( root, nodes, values );
-    if( nodes.size() <= 1 ) 
+    List<TreeNode> nodes = new ArrayList<TreeNode>();
+    List<Integer> values = new ArrayList<Integer>();
+    convertToArray(root, nodes, values);
+    if (nodes.size() < 1) {
       return;
+    }
     Collections.sort(values);
     for( int i = 0; i < nodes.size(); ++i )
       nodes.get(i).val = values.get(i);
     return;
   }
-  public void convert2array( TreeNode root, ArrayList<TreeNode> nodes, ArrayList<Integer> values ) {
-    if( root == null ) return;
-    convert2array( root.left, nodes, values );
-    nodes.add( root );
-    values.add( root.val );
-    convert2array( root.right, nodes, values );
+  public void convertToArray(TreeNode root, List<TreeNode> nodes, List<Integer> values) {
+    if (root == null) {
+      return;
+    }
+    convertToArray(root.left, nodes, values);
+    nodes.add(root);
+    values.add(root.val);
+    convertToArray(root.right, nodes, values);
   }
 }
 
@@ -47,28 +48,28 @@ public class Solution {
   TreeNode firstNode = null;
   TreeNode secondNode = null;
   public void recoverTree(TreeNode root) {
-    if( root == null )
-      return;
-    lastNode = new TreeNode(Integer.MIN_VALUE);
     firstNode = null;
     secondNode = null;
-    _recoverTree(root);
+    lastNode = new TreeNode(Integer.MIN_VALUE);
+    recoverTreeHelper(root);
     int tmp = firstNode.val;
     firstNode.val = secondNode.val;
     secondNode.val = tmp;
   }
-  public void _recoverTree(TreeNode root) {
-    if( root == null )
+  public void recoverTreeHelper(TreeNode root) {
+    if (root == null) {
       return;
-    _recoverTree(root.left);
-    if( firstNode == null && root.val < lastNode.val ) {
-      firstNode = root;
     }
-    if( firstNode != null && root.val < lastNode.val ) {
+    recoverTreeHelper(root.left);
+    if (firstNode == null && root.val < lastNode.val) {
+      firstNode = lastNode;
+    }
+    if (firstNode != null && root.val < lastNode.val) {
       secondNode = root;
     }
     lastNode = root;
-    _recoverTree(root.right);
+    recoverTreeHelper(root.right);
   }
 }
+
 
