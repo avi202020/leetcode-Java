80e0e29eed3b7a1faf77994f9015d6373cabff47
jintaoguan <ilovexiao77@gmail.com>
2016-08-10 18:14:38 -0700

    update Letter Combinations of a Phone Number

--- Letter_Combinations_of_a_Phone_Number.java
+++ Letter_Combinations_of_a_Phone_Number.java
@@ -1,36 +1,36 @@
 public class Solution {
-	public ArrayList<String> letterCombinations(String digits) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<String> ans = new ArrayList<String>();
-		StringBuffer sb = new StringBuffer();
-		ArrayList<String> table = new ArrayList<String>();
-		table.add("");
-		table.add("");
-		table.add("abc");
-		table.add("def");
-		table.add("ghi");
-		table.add("jkl");
-		table.add("mno");
-		table.add("qprs");
-		table.add("tuv");
-		table.add("wxyz");
-		dfs( digits, 0, table, sb, ans );
-		return ans;
-	}
-	public void dfs( String digits, int depth, ArrayList<String> table, StringBuffer sb, ArrayList<String> ans )
-	{
-		if( depth >= digits.length() )
-		{
-			ans.add( sb.toString() );
-			return;
-		}
-		int index = digits.charAt(depth) - '0';
-		for( int i = 0; i < table.get(index).length(); ++i )
-		{
-			sb.append( table.get(index).charAt(i) );
-			dfs( digits, depth + 1, table, sb, ans );
-			sb.deleteCharAt( sb.length() - 1 );
-		}
-	}
+  public List<String> letterCombinations(String digits) {
+    List<String> res = new ArrayList<String>();
+    if (digits.length() <= 0) {
+      return res;
+    }
+    List<String> table = new ArrayList<String>();
+    table.add("");
+    table.add("");
+    table.add("abc");
+    table.add("def");
+    table.add("ghi");
+    table.add("jkl");
+    table.add("mno");
+    table.add("qprs");
+    table.add("tuv");
+    table.add("wxyz");
+    StringBuilder sb = new StringBuilder();
+    letterCombinationsHelper(digits, 0, table, res, sb);
+    return res;
+  }
+  public void letterCombinationsHelper(String digits, int depth, List<String> table, List<String> res, StringBuilder path) {
+    if (depth >= digits.length()) {
+      res.add(path.toString());
+      return;
+    }
+    int digit = digits.charAt(depth) - '0';
+    String options = table.get(digit);
+    for (int i = 0; i < options.length(); ++i) {
+      char ch = options.charAt(i);
+      path.append(ch);
+      letterCombinationsHelper(digits, depth + 1, table, res, path);
+      path.deleteCharAt(path.length() - 1);
+    }
+  }
 }
