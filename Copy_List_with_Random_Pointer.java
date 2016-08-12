684dfcc3982fce69acc85022b09e546fec9f559c
jintaoguan <ilovexiao77@gmail.com>
2016-08-11 20:27:15 -0700

    update Copy List with Random Pointer

--- Copy_List_with_Random_Pointer.java
+++ Copy_List_with_Random_Pointer.java
@@ -9,32 +9,30 @@
 
 // Solution No.1   O(n) time, O(n) space
 public class Solution {
-	public RandomListNode copyRandomList(RandomListNode head) {
-		// IMPORTANT: Please reset any member data you declared, as
-		// the same Solution instance will be reused for each test case.
-		if( head == null ) return null;
-		HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
-		RandomListNode old_head = new RandomListNode(-1);
-		old_head.next = head;
-		RandomListNode new_head = new RandomListNode(-1);
-		RandomListNode p1 = old_head.next;
-		RandomListNode p2 = new_head;
-		while( p1 != null ) {
-			RandomListNode node = new RandomListNode(p1.label);
-			p2.next = node;
-			p2 = node;
-			map.put( p1, p2 );
-			p1 = p1.next;
-		}
-		p1 = old_head.next;
-		p2 = new_head.next;
-		while( p2 != null ) {
-			p2.random = map.get(p1.random);
-			p2 = p2.next;
-			p1 = p1.next;
-		}
-		return new_head.next;
-	}
+  public RandomListNode copyRandomList(RandomListNode head) {
+    if (head == null) {
+      return head;
+    }
+    Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+    RandomListNode dummy = new RandomListNode(-1);
+    RandomListNode pre = dummy;
+    RandomListNode cur = head;
+    while (cur != null) {
+      RandomListNode node = new RandomListNode(cur.label);
+      pre.next = node;
+      pre = node;
+      map.put(cur, node);
+      cur = cur.next;
+    }
+    RandomListNode p1 = head;
+    RandomListNode p2 = dummy.next;
+    while (p1 != null) {
+      p2.random = map.get(p1.random);
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    return dummy.next;
+  }
 }
 
 // Solution No.2   O(n) time, O(1) space
