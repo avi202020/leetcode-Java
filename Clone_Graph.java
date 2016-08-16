1a4be1bc7bf18c759598337bc4e8bf29c2454a20
jintaoguan <ilovexiao77@gmail.com>
2016-08-16 15:30:52 -0700

    update Clone Graph

--- Clone_Graph.java
+++ Clone_Graph.java
@@ -69,30 +69,28 @@ public class Solution {
 
 // Solution 2: bfs
 public class Solution {
-	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-		// Note: The Solution object is instantiated only once and is reused by each test case.
-		if( node == null ) return null;
-		HashMap<UndirectedGraphNode,UndirectedGraphNode> map = new HashMap<UndirectedGraphNode,UndirectedGraphNode>();
-		Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
-		queue.offer(node);
-		UndirectedGraphNode newnode = new UndirectedGraphNode(node.label);
-		map.put(node,newnode);
-		bfs( queue, map );
-		return newnode;
-	}
-	public void bfs( Queue<UndirectedGraphNode> queue, HashMap<UndirectedGraphNode,UndirectedGraphNode> map ) {
-		while( !queue.isEmpty() ) {
-			UndirectedGraphNode node = queue.poll();
-			for( int i = 0; i < node.neighbors.size(); ++i ) {
-				if( !map.containsKey(node.neighbors.get(i)) ) {
-					UndirectedGraphNode newnode = new UndirectedGraphNode(node.neighbors.get(i).label);
-					map.put(node.neighbors.get(i), newnode);
-					map.get(node).neighbors.add(newnode);
-					queue.offer(node.neighbors.get(i));
-				} else {
-					map.get(node).neighbors.add(map.get(node.neighbors.get(i)));
-				}
-			}
-		}
-	}
+  public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    if (node == null) {
+      return null;
+    }
+    Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+    Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+    queue.add(node);
+    UndirectedGraphNode res = new UndirectedGraphNode(node.label);
+    map.put(node, res);
+    while (!queue.isEmpty()) {
+      UndirectedGraphNode graphNode = queue.poll();
+      for (int i = 0; i < graphNode.neighbors.size(); ++i) {
+        if (!map.containsKey(graphNode.neighbors.get(i))) {
+          UndirectedGraphNode newNode = new UndirectedGraphNode(graphNode.neighbors.get(i).label);
+          map.put(graphNode.neighbors.get(i), newNode);
+          map.get(graphNode).neighbors.add(newNode);
+          queue.offer(graphNode.neighbors.get(i));
+        } else {
+          map.get(graphNode).neighbors.add(map.get(graphNode.neighbors.get(i)));
+        }
+      }
+    }
+    return res;
+  }
 }
