44f7f0bf452a4e57951611c0aa015319ea21541d
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 21:13:17 -0700

    update First Missing Positive

--- First_Missing_Positive.java
+++ First_Missing_Positive.java
@@ -1,28 +1,26 @@
 public class Solution {
-    public int firstMissingPositive(int[] A) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int i = 0;
-        int n = A.length;
-        while (i < n) {
-            if( A[i] < 1 || A[i] >= n || A[i] == i + 1 ) {
-                i++;
-            }
-            else if( A[i] != i + 1 ) {
-                if( A[A[i]-1] != A[i] ) {
-                    int pos = A[i] - 1;
-                    int tmp = A[i];
-                    A[i] = A[pos];
-                    A[pos] = tmp;
-                }
-                else 
-                    i++;
-            }
-
+  public int firstMissingPositive(int[] nums) {
+    int index = 0;
+    int n = nums.length;
+    while (index < n) {
+      if (nums[index] < 1 || nums[index] >= n || nums[index] == index + 1) {
+        index++;
+      } else if (nums[index] != index + 1) {
+        if (nums[nums[index]-1] != nums[index]) {
+          int pos = nums[index] - 1;
+          int tmp = nums[index];
+          nums[index] = nums[pos];
+          nums[pos] = tmp;
+        } else {
+          index++;
         }
-        for (i = 0; i < n; ++i)
-            if ( A[i] != i + 1 )
-                return i + 1;
-        return n + 1;
+      }
     }
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+    return n + 1;
+  }
 }
