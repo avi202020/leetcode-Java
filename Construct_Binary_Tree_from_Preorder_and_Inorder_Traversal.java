a3701b90f306d568219d5c366da2bb97927cb064
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 17:08:57 -0700

    update Construct Binary Tree from Preorder and Inorder Traversal

--- Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -8,23 +8,23 @@
  * }
  */
 public class Solution {
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
-		// Start typing your Java solution belowr
-		// DO NOT write main() function
-		HashMap<Integer,Integer> posMap = new HashMap<Integer,Integer>();
-		for( int i = 0; i < inorder.length; ++i )
-			posMap.put(inorder[i], i);
-		TreeNode root = _buildTree( preorder, 0, 0, preorder.length, posMap );
-		return root;
-	}
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    Map<Integer,Integer> positions = new HashMap<Integer,Integer>();
+    for (int i = 0; i < inorder.length; ++i) {
+      positions.put(inorder[i], i);
+    }
+    TreeNode root = buildTreeHelper(preorder, 0, 0, preorder.length, positions);
+    return root;
+  }
 
-	public TreeNode _buildTree(int[] pre, int index, int start, int n, HashMap<Integer, Integer> posMap ) {
-		if( n == 0 )
-			return null;
-		TreeNode root = new TreeNode( pre[index] );
-		int length = posMap.get( pre[index] ) - start;
-		root.left = _buildTree( pre, index + 1, start, length, posMap );
-		root.right = _buildTree( pre, index + length + 1, start + length + 1, n - length - 1, posMap );
-		return root;
-	}
+  public TreeNode buildTreeHelper(int[] pre, int index, int start, int n, Map<Integer, Integer> posMap) {
+    if (n == 0) {
+      return null;
+    }
+    TreeNode root = new TreeNode(pre[index]);
+    int length = posMap.get(pre[index]) - start;
+    root.left = buildTreeHelper(pre, index + 1, start, length, posMap);
+    root.right = buildTreeHelper(pre, index + length + 1, start + length + 1, n - length - 1, posMap);
+    return root;
+  }
 }
