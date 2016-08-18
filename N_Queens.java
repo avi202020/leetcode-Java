d5437425c03eef3b510a3c141b271910b821d160
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 23:29:45 -0700

    udpate N Queens

--- N_Queens.java
+++ N_Queens.java
@@ -1,44 +1,41 @@
 public class Solution {
-	public ArrayList<String[]> solveNQueens(int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<String[]> ans = new ArrayList<String[]>();
-		int[] queen = new int[n];
-		dfs(ans, queen, 0);
-		return ans;
-	}
-
-	public void dfs(ArrayList<String[]> ans, int[] queen, int depth) {
-		int maxd = queen.length;
-		if (depth >= maxd) {
-			String[] board = new String[maxd];
-			for (int i = 0; i < maxd; ++i) {
-				StringBuffer sb = new StringBuffer();
-				for (int j = 0; j < maxd; ++j) {
-					if (queen[i] == j)
-						sb.append('Q');
-					else
-						sb.append('.');
-				}
-				board[i] = sb.toString();
-			}
-			ans.add(board);
-		}
-		for (int i = 0; i < maxd; ++i) {
-			if (check(queen, depth, i)) {
-				queen[depth] = i;
-				dfs(ans, queen, depth + 1);
-			}
-		}
-	}
-
-	public boolean check(int[] queen, int x, int y) {
-		for (int i = 0; i < x; ++i) {
-			if (queen[i] == y)
-				return false;
-			else if (Math.abs(queen[i] - y) == Math.abs(x - i))
-				return false;
-		}
-		return true;
-	}
+  public List<List<String>> solveNQueens(int n) {
+    List<List<String>> ans = new ArrayList<List<String>>();
+    int[] queen = new int[n];
+    solveNQueensHelper(ans, queen, 0);
+    return ans;
+  }
+  private void solveNQueensHelper(List<List<String>> ans, int[] queen, int depth) {
+    if (depth >= queen.length) {
+      List<String> one = new ArrayList<String>();
+      for (int i = 0; i < queen.length; ++i) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < queen.length; ++j) {
+          if (queen[i] == j) {
+            sb.append("Q");
+          } else {
+            sb.append(".");
+          }
+        }
+        one.add(sb.toString());
+      }
+      ans.add(one);
+    }
+    for (int i = 0; i < queen.length; ++i) {
+      if (check(queen, depth, i)) {
+        queen[depth] = i;
+        solveNQueensHelper(ans, queen, depth + 1);
+      }
+    }
+  }
+  private boolean check(int[] queen, int x, int y) {
+    for (int i = 0; i < x; ++i) {
+      if (queen[i] == y) {
+        return false;
+      } else if (Math.abs(queen[i] - y) == Math.abs(x - i)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
