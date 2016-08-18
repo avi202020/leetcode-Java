36e6e8b89a61be937f943d6b0d2ee2902c2c512d
jintaoguan <ilovexiao77@gmail.com>
2016-08-18 00:12:16 -0700

    update Subsets

--- Subsets.java
+++ Subsets.java
@@ -1,26 +1,23 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> line = new ArrayList<Integer>();
-		Arrays.sort(S);
-		dfs( S, 0, ans, line );
-		return ans;
-	}
-	public void dfs( int[] arr, int depth, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> line )
-	{
-		if( depth >= arr.length )
-		{
-			ArrayList<Integer> tmp = new ArrayList<Integer>(line);
-			ans.add(tmp);
-			return;
-		}
-		//take this element
-		line.add( arr[depth] );
-		dfs( arr, depth + 1, ans, line );
-		line.remove( line.size() - 1 );
-		//do not take this element
-		dfs( arr, depth + 1, ans, line );
-	}
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if (nums == null || nums.length == 0) {
+      return res;
+    }
+    List<Integer> line = new ArrayList<Integer>();
+    Arrays.sort(nums);
+    subsetsHelper(nums, 0, line, res);
+    return res;
+  }
+  private void subsetsHelper(int[] nums, int depth, List<Integer> line, List<List<Integer>> res) {
+    if (depth >= nums.length) {
+      List<Integer> subset = new ArrayList<Integer>(line);
+      res.add(subset);
+      return;
+    }
+    subsetsHelper(nums, depth + 1, line, res);
+    line.add(nums[depth]);
+    subsetsHelper(nums, depth + 1, line, res);
+    line.remove(line.size() - 1);
+  }
 }
