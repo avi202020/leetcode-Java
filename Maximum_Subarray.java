05f43cd087d6fc4e5a2e7c3b8497f5b45bd4ded7
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 22:59:59 -0700

    update Maximum Subarray

--- Maximum_Subarray.java
+++ Maximum_Subarray.java
@@ -1,18 +1,17 @@
 public class Solution {
-	public int maxSubArray(int[] A) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int max = Integer.MIN_VALUE;
-		int sum = 0;
-		int maxsum = 0;
-		for( int i = 0; i < A.length; ++i )
-		{
-			if( A[i] > max ) max = A[i];
-			sum += A[i];
-			if( sum < 0 ) sum = 0;
-			if( sum > maxsum ) maxsum = sum;
-		}
-		if( max < 0 ) return max;
-		return maxsum;
-	}
+  public int maxSubArray(int[] nums) {
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    int maxsum = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      max = Math.max(max, nums[i]);
+      sum += nums[i];
+      sum = Math.max(0, sum);
+      maxsum = Math.max(maxsum, sum);
+    }
+    if (max < 0) {
+      return max;
+    }
+    return maxsum;
+  }
 }
