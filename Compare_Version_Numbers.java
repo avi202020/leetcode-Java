cdfc060b010f4ae2e75814d81da5845e5d78d2fc
jintaoguan <ilovexiao77@gmail.com>
2016-08-18 16:09:00 -0700

    add Compare Version Numbers

--- Compare_Version_Numbers.java
+++ Compare_Version_Numbers.java
@@ -1,17 +1,23 @@
 public class Solution {
   public int compareVersion(String version1, String version2) {
-    String[] numbers1 = version1.split("\\.");
-    String[] numbers2 = version2.split("\\.");
-    int len = Math.max(numbers1.length, numbers2.length);
-    int cur = 0;
-    while (cur < len) {
-      int v1 = cur < numbers1.length ? Integer.valueOf(numbers1[cur]) : 0;
-      int v2 = cur < numbers2.length ? Integer.valueOf(numbers2[cur]) : 0;
-      if (v1 < v2)
-        return -1;
-      else if (v1 > v2)
+    String[] str1 = version1.split("\\.");
+    String[] str2 = version2.split("\\.");
+    int maxLen = Math.max(str1.length, str2.length);
+    for (int i = 0; i < maxLen; ++i) {
+      int digit1 = 0;
+      int digit2 = 0;
+      if (i < str1.length) {
+        digit1 = Integer.parseInt(str1[i]);
+      }
+      if (i < str2.length) {
+        digit2 = Integer.parseInt(str2[i]);
+      }
+      if (digit1 > digit2) {
         return 1;
-      cur++;
+      }
+      if (digit1 < digit2) {
+        return -1;
+      }
     }
     return 0;
   }
