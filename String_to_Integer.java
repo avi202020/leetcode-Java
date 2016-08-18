2cdf3a1ea5a4d4f356d19d3a65c3420fa4470e6b
jintaoguan <ilovexiao77@gmail.com>
2016-08-18 13:11:44 -0700

    update String to Integer (atoi)

--- String_to_Integer.java
+++ String_to_Integer.java
@@ -1,35 +1,37 @@
 public class Solution {
-	public int atoi(String str) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( str == null || str.equals("") )
-			return 0;
-		long ans = 0;
-		int index = 0;
-		boolean positive = true;
-		while( str.charAt(index) == ' ' )
-			index++;
-		if( str.charAt(index) == '-' ) {
-			positive = false;
-			index++;
-		}
-		else if( str.charAt(index) == '+' ) {
-			positive = true;
-			index++;
-		}
-		while( index < str.length() ) {
-			if( !Character.isDigit( str.charAt(index) ) )
-				break;
-			int num = (int)( str.charAt(index) - '0' );
-			ans = ans * 10 + num;
-			index++;
-		}
-		if( positive == false )
-			ans = -ans;
-		if( ans > Integer.MAX_VALUE )
-			return Integer.MAX_VALUE;
-		else if( ans < Integer.MIN_VALUE )
-			return Integer.MIN_VALUE;
-		return (int)ans;
-	}
+  public int myAtoi(String str) {
+    if (str == null || str.equals("")) {
+      return 0;
+    }
+    long ans = 0;
+    int index = 0;
+    int positive = 1;
+    while (str.charAt(index) == ' ') {
+      index++;
+    }
+    if (str.charAt(index) == '-') {
+      positive = -1;
+      index++;
+    } else if(str.charAt(index) == '+') {
+      positive = 1;
+      index++;
+    }
+    while (index < str.length()) {
+      if (!Character.isDigit(str.charAt(index))) {
+        break;
+      }
+      int num = (int)(str.charAt(index) - '0');
+      ans = ans * 10 + num;
+      index++;
+      if (positive > 0 && ans * positive > Integer.MAX_VALUE) {
+        return Integer.MAX_VALUE;
+      } else if (positive < 0 && ans * positive < Integer.MIN_VALUE) {
+        return Integer.MIN_VALUE;
+      }
+    }
+    if (positive == -1) {
+      ans = -ans;
+    }
+    return (int)ans;
+  }
 }
