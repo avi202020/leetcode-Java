a6ed509cf4965a6eda742ab1afb20bc437d9c6c8
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 23:06:10 -0700

    update Maximum Product Subarray

--- Maximum_Product_Subarray.java
+++ Maximum_Product_Subarray.java
@@ -1,15 +1,16 @@
 public class Solution {
-  public int maxProduct(int[] A) {
-    if( A == null || A.length == 0 )
+  public int maxProduct(int[] nums) {
+    if (nums == null || nums.length == 0) {
       return 0;
-    int ans = A[0];
-    int maxPositive = A[0];
-    int minNegative = A[0];
-    for( int i = 1; i < A.length; ++i ) {
-      int next_max = Math.max( A[i] * maxPositive, Math.max(A[i] * minNegative, A[i] ) );
-      int next_min = Math.min( A[i] * maxPositive, Math.min(A[i] * minNegative, A[i] ) );
-      maxPositive = next_max;
-      minNegative = next_min;
+    }
+    int ans = nums[0];
+    int maxPositive = nums[0];
+    int minNegative = nums[0];
+    for (int i = 1; i < nums.length; ++i) {
+      int nextMax = Math.max(nums[i] * maxPositive, Math.max(nums[i] * minNegative, nums[i]));
+      int nextMin = Math.min(nums[i] * minNegative, Math.min(nums[i] * maxPositive, nums[i]));
+      maxPositive = nextMax;
+      minNegative = nextMin;
       ans = Math.max(ans, maxPositive);
     }
     return ans;
