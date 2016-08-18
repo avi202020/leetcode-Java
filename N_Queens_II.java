16a2ecadec49b8120c5c3bfdd5a13c2668cdd211
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 23:34:19 -0700

    update N-Queens II

--- N_Queens_II.java
+++ N_Queens_II.java
@@ -1,38 +1,32 @@
 public class Solution {
-	public int count;
-	public int totalNQueens(int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int[] queen = new int[n];
-		this.count = 0;
-		dfs( queen, 0 );
-		return this.count;
-	}
-
-	public void dfs( int[] queen, int depth )
-	{
-		int maxd = queen.length;
-		if( depth >= maxd )
-			this.count++;
-		for( int i = 0; i < maxd; ++i )
-		{
-			if( check( queen, depth, i ) )
-			{
-				queen[depth] = i;
-				dfs( queen, depth + 1 );
-			}
-		}
-	}
-
-	public boolean check( int[] queen, int x, int y )
-	{
-		for( int i = 0; i < x; ++i )
-		{
-			if( queen[i] == y ) 
-				return false;
-			else if( Math.abs( queen[i] - y ) == Math.abs( x - i ) )
-				return false;
-		}
-		return true;
-	}
+  private int totalSolutions;
+  public int totalNQueens(int n) {
+    int[] queen = new int[n];
+    totalSolutions = 0;
+    totalNQueensHelper(queen, 0);
+    return totalSolutions;
+  }
+  private void totalNQueensHelper(int[] queen, int depth) {
+    if (depth >= queen.length) {
+      totalSolutions++;
+      return;
+    }
+    for (int i = 0; i < queen.length; ++i) {
+      if (check(queen, depth, i)) {
+        queen[depth] = i;
+        totalNQueensHelper(queen, depth + 1);
+        queen[depth] = 0;
+      }
+    }
+  }
+  private boolean check(int[] queen, int x, int y) {
+    for (int i = 0; i < x; ++i) {
+      if (queen[i] == y) {
+        return false;
+      } else if (Math.abs(queen[i] - y) == Math.abs(x - i)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
