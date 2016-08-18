e92df4aa486f631a960c2080403c4aeba9eeb151
jintaoguan <ilovexiao77@gmail.com>
2016-08-17 19:19:21 -0700

    update Next Permutation

--- Next_Permutation.java
+++ Next_Permutation.java
@@ -1,39 +1,38 @@
 public class Solution {
-	public void nextPermutation(int[] num) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int len = num.length;
-		int i = len - 1;
-		while( i >= 1 && num[i] <= num[i-1] )
-			i--;
-		if( i == 0 )
-		{
-			int left = 0;
-			int right = len - 1;
-			while( left <= right )
-			{
-				int tmp = num[left];
-				num[left] = num[right];
-				num[right] = tmp;
-				left++;
-				right--;
-			}
-			return;
-		}
-		int j = len - 1;
-		while( num[j] <= num[i-1] )
-			j--;
-		int tmp = num[j];
-		num[j] = num[i-1];
-		num[i-1] = tmp;
-		j = len - 1;
-		while (i < j) 
-		{
-			tmp = num[i];
-			num[i] = num[j];
-			num[j] = tmp;
-			i++;
-			j--;
-		}
-	}
+  public void nextPermutation(int[] num) {
+    int len = num.length;
+    int i = len - 1;
+    // num[i] is the peak
+    while (i >= 1 && num[i] <= num[i-1]) {
+      i--;
+    }
+    // descending order
+    if (i == 0) {
+      int left = 0;
+      int right = len - 1;
+      while( left <= right ) {
+        int tmp = num[left];
+        num[left] = num[right];
+        num[right] = tmp;
+        left++;
+        right--;
+      }
+      return;
+    }
+    int j = len - 1;
+    while (num[j] <= num[i-1]) {
+      j--;
+    }
+    int tmp = num[j];
+    num[j] = num[i-1];
+    num[i-1] = tmp;
+    j = len - 1;
+    while (i < j) {
+      tmp = num[i];
+      num[i] = num[j];
+      num[j] = tmp;
+      i++;
+      j--;
+    }
+  }
 }
