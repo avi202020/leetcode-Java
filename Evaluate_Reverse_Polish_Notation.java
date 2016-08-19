1e6bed37edfcc7825f363a4e74df147ca5791613
jintaoguan <ilovexiao77@gmail.com>
2016-08-19 13:40:28 -0700

    update Evaluate Reverse Polish Notation

--- Evaluate_Reverse_Polish_Notation.java
+++ Evaluate_Reverse_Polish_Notation.java
@@ -1,51 +1,53 @@
 public class Solution {
-	public int evalRPN(String[] tokens) {
-		if( tokens.length == 1 ) 
-			return Integer.parseInt(tokens[0]);
-		else if( tokens.length < 3 ) return 0;
-		Stack<Integer> stack = new Stack<Integer>();
-		stack.push( Integer.parseInt(tokens[0]) );
-		stack.push( Integer.parseInt(tokens[1]) );
-		for( int i = 2; i < tokens.length; ++i ) {
-			if( isOperator(tokens[i]) ) {
-				if( tokens[i].equals("+") ) {
-					int num1 = stack.pop();
-					int num2 = stack.pop();
-					int num3 = num2 + num1;
-					stack.push(num3);
-				}
-				if( tokens[i].equals("-") ) {
-					int num1 = stack.pop();
-					int num2 = stack.pop();
-					int num3 = num2 - num1;
-					stack.push(num3);
-				}
-				if( tokens[i].equals("*") ) {
-					int num1 = stack.pop();
-					int num2 = stack.pop();
-					int num3 = num2 * num1;
-					stack.push(num3);
-				}
-				if( tokens[i].equals("/") ) {
-					int num1 = stack.pop();
-					int num2 = stack.pop();
-					int num3 = num2 / num1;
-					stack.push(num3);
-				}
-
-			}
-			else if( !isOperator(tokens[i]) ) {
-				stack.push(Integer.parseInt(tokens[i]));
-			}
-		}
-		int ans = stack.pop();
-		return ans;
-	}
-	public boolean isOperator( String str ) {
-		if( str.equals("+") ) return true;
-		if( str.equals("-") ) return true;
-		if( str.equals("*") ) return true;
-		if( str.equals("/") ) return true;
-		return false;
-	}
+  public int evalRPN(String[] tokens) {
+    if (tokens.length == 1) {
+      return Integer.parseInt(tokens[0]);
+    } else if (tokens.length < 3) {
+      return 0;
+    }
+    Stack<Integer> stack = new Stack<Integer>();
+    stack.push(Integer.parseInt(tokens[0]));
+    stack.push(Integer.parseInt(tokens[1]));
+    for (int i = 2; i < tokens.length; ++i) {
+      if (isOperator(tokens[i])) {
+        if (tokens[i].equals("+")) {
+          int num1 = stack.pop();
+          int num2 = stack.pop();
+          int num3 = num2 + num1;
+          stack.push(num3);
+        } else if (tokens[i].equals("-")) {
+          int num1 = stack.pop();
+          int num2 = stack.pop();
+          int num3 = num2 - num1;
+          stack.push(num3);
+        } else if (tokens[i].equals("*")) {
+          int num1 = stack.pop();
+          int num2 = stack.pop();
+          int num3 = num2 * num1;
+          stack.push(num3);
+        } else if (tokens[i].equals("/")) {
+          int num1 = stack.pop();
+          int num2 = stack.pop();
+          int num3 = num2 / num1;
+          stack.push(num3);
+        }
+      } else {
+        stack.push(Integer.parseInt(tokens[i]));
+      }
+    }
+    int ans = stack.pop();
+    return ans;
+  }
+  public boolean isOperator(String str) {
+    if (str.equals("+")) {
+      return true;
+    } else if (str.equals("-")) {
+      return true;
+    } else if (str.equals("*")) {
+      return true;
+    } else if (str.equals("/")) {
+      return true;
+    }
+    return false;
+  }
 }
