0832f37af72020719e7391bc68857fbe7f9fafaa
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 00:19:47 -0700

    update Minimum Path Sum

--- Minimum_Path_Sum.java
+++ Minimum_Path_Sum.java
@@ -1,24 +1,25 @@
 public class Solution {
-    public int minPathSum(int[][] grid) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int m = grid.length;
-        if( m == 0 ) return 0;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        for( int i = 0; i < m; ++i )
-        {
-            for( int j = 0; j < n; ++j )
-            {
-                if( i == 0 && j == 0 ) continue;
-                int left = Integer.MAX_VALUE;
-                int up = Integer.MAX_VALUE;
-                if( j != 0 ) left = dp[i][j-1];
-                if( i != 0 ) up = dp[i-1][j];
-                dp[i][j] = Math.min( left, up ) + grid[i][j];
-            }
-        }
-        return dp[m-1][n-1];
+  public int minPathSum(int[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
     }
+    int m = grid.length;
+    int n = grid[0].length;
+    int[][] dp = new int[m][n];
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i == 0 && j == 0) {
+          dp[i][j] = grid[i][j];
+          continue;
+        } else if (i == 0) {
+          dp[i][j] = dp[i][j-1] + grid[i][j];
+        } else if (j == 0) {
+          dp[i][j] = dp[i-1][j] + grid[i][j];
+        } else {
+          dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+        }
+      }
+    }
+    return dp[m-1][n-1];
+  }
 }
