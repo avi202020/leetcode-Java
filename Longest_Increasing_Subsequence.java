d61f9474b39e6c59223fa73a92726000f79033b7
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 00:09:46 -0700

    update Longest Increasing Subsequence

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -21,3 +21,37 @@ public class Solution {
     return max;
   }
 }
+
+// Solution 2
+public class Solution {
+  public int lengthOfLIS(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int[] dp = new int[nums.length+1];
+    dp[1] = nums[0];
+    int top = 1;
+    for (int i = 1; i < nums.length; ++i) {
+      int low = 1;
+      int high = top;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (dp[mid] == nums[i]) {
+          break;
+        } else if (dp[mid] < nums[i]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
+      if (low <= high) {
+        continue;
+      } else if (high + 1 > top) {
+        dp[++top] = nums[i];
+      } else {
+        dp[high + 1] = nums[i];
+      }
+    }
+    return top;
+  }
+}
