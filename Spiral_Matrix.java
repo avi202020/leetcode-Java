d04ec38623f47be5b933ab00cfad5915662a75a5
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 23:14:34 -0700

    update 54. Spiral Matrix

--- Spiral_Matrix.java
+++ Spiral_Matrix.java
@@ -1,33 +1,30 @@
 public class Solution {
-
-    // four directions
-    int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-    public ArrayList<Integer> spiralOrder(int[][] matrix) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        if (matrix.length == 0)
-            return ans;
-        boolean[][] visit = new boolean[matrix.length][matrix[0].length];
-        dfs(matrix, visit, 0, 0, 0, ans);
-        return ans;
+   public List<Integer> spiralOrder(int[][] matrix) {
+    int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    List<Integer> res = new ArrayList<Integer>();
+    if (matrix == null || matrix.length == 0) {
+      return res;
     }
-
-    public void dfs(int[][] matrix, boolean[][] visit, int x, int y, int curdir, ArrayList<Integer> ans) {
-        if (ans.size() >= matrix.length * matrix[0].length)
-            return;
-        ans.add(matrix[x][y]);
-        visit[x][y] = true;
-        int nx = x + dir[curdir][0];
-        int ny = y + dir[curdir][1];
-        if (nx >= 0 && nx < matrix.length && ny >= 0 && ny < matrix[0].length && visit[nx][ny] == false) {
-            dfs(matrix, visit, nx, ny, curdir, ans);
-        } else {
-            curdir = (curdir + 1) % 4;
-            nx = x + dir[curdir][0];
-            ny = y + dir[curdir][1];
-            dfs(matrix, visit, nx, ny, curdir, ans);
-        }
+    int m = matrix.length;
+    int n = matrix[0].length;
+    boolean[][] visited = new boolean[m][n];
+    spiralOrderHelper(matrix, 0, 0, dirs, 0, visited, res);
+    return res;
+  }
+  private void spiralOrderHelper(int[][] matrix, int x, int y, int[][] dirs, int dir,
+                                          boolean[][] visited, List<Integer> res) {
+    if (res.size() >= matrix.length * matrix[0].length) {
+      return;
     }
+    res.add(matrix[x][y]);
+    visited[x][y] = true;
+    int nx = x + dirs[dir][0];
+    int ny = y + dirs[dir][1];
+    if (nx < 0 || nx >= matrix.length || ny < 0 || ny >= matrix[0].length || visited[nx][ny]) {
+      dir = (dir + 1) % 4;
+      nx = x + dirs[dir][0];
+      ny = y + dirs[dir][1];
+    }
+    spiralOrderHelper(matrix, nx, ny, dirs, dir, visited, res);
+  }
 }
