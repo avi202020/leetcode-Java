d7d6bcbcaa8db5ef7a670b4643ac890f642fee27
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 02:05:29 -0700

    update 215. Kth Largest Element in an Array

--- Kth_Largest_Element_in_an_Array.java
+++ Kth_Largest_Element_in_an_Array.java
@@ -1,4 +1,46 @@
-// Solution 1. heap
+// Solution 1. quick select
+public class Solution {
+  public int findKthLargest(int[] nums, int k) {
+    if (k < 1 || nums == null || nums.length == 0) {
+      return 0;
+    }
+    return findKthLargestHelper(nums, nums.length - k + 1, 0, nums.length - 1);
+  }
+
+  private int findKthLargestHelper(int[] nums, int k, int lo, int hi) {
+    int pivot = nums[hi];
+    int left = lo;
+    int right = hi;
+    while (true) {
+      while (nums[left] < pivot && left < right) {
+        left++;
+      }
+      while (nums[right] >= pivot && right > left) {
+        right--;
+      }
+      if (left >= right) {
+        break;
+      }
+      swap(nums, left, right);
+    }
+    swap(nums, left, hi);
+    if (k == left + 1) {
+      return pivot;
+    } else if (k < left + 1) {
+      return findKthLargestHelper(nums, k, lo, left - 1);
+    } else {
+      return findKthLargestHelper(nums, k, left + 1, hi);
+    }
+  }
+  
+  private void swap(int[] nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+}
+
+// Solution 2. heap
 public class Solution {
   public int findKthLargest(int[] nums, int k) {
     PriorityQueue<Integer> heap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
