b0d829746807ed719b9601900d462e45890ff85c
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 13:37:30 -0700

    update 4. Median of Two Sorted Arrays

--- Median_of_Two_Sorted_Arrays.java
+++ Median_of_Two_Sorted_Arrays.java
@@ -3,22 +3,23 @@ public class Solution {
   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int n = nums1.length;
     int m = nums2.length;
-    double[] nums3 = new double[n+m];
+    double[] nums3 = new double[n + m];
     int index = 0;
     for (int i = 0; i < n; ++i) {
       nums3[index++] = nums1[i];
     }
-    for( int i = 0; i < m; ++i )
+    for (int i = 0; i < m; ++i) {
       nums3[index++] = nums2[i];
+    }
     Arrays.sort(nums3);
-    if (nums3.length % 2 != 0)
-      return nums3[nums3.length/2];
-    double ans1 = nums3[nums3.length/2-1];
-    double ans2 = nums3[nums3.length/2];
-    return (ans1 + ans2)/2;
+    if (nums3.length % 2 != 0) {
+      return nums3[nums3.length / 2];
+    }
+    double ans1 = nums3[nums3.length / 2 - 1];
+    double ans2 = nums3[nums3.length / 2];
+    return (ans1 + ans2) / 2;
   }
 }
-
 
 //  2. Better solution
 public class Solution {
