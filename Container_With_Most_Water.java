941e738d849322e4fc5b732f783114b19f37685d
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 13:17:04 -0700

    update 11. Container With Most Water

--- Container_With_Most_Water.java
+++ Container_With_Most_Water.java
@@ -1,36 +1,3 @@
-//************************************************************
-//                  1. naive method
-//************************************************************
-public class Solution {
-	public int maxArea(int[] height) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int maxarea = Integer.MIN_VALUE;
-		int maxheight = Integer.MIN_VALUE;
-		for( int i = 0; i < height.length - 1; ++i )
-		{
-			int j = height.length - 1;
-			if( maxheight < height[i] )
-				maxheight = height[i];
-			else continue;
-			while( i < j )
-			{
-				int area = Math.min( height[i], height[j] ) * ( j - i );
-				if( area > maxarea ) maxarea = area;
-				j--;
-				if( height[i] <= height[j] )
-					break;
-			}
-			int area = height[i] * ( j - i );
-			if( area > maxarea ) maxarea = area;
-		}
-		return maxarea;
-	}
-}
-
-//************************************************************
-//                  2. better method
-//************************************************************
 public class Solution {
   public int maxArea(int[] height) {
     int maxarea = Integer.MIN_VALUE;
