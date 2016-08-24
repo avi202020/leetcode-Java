ec46f999408c9d1ab978fbb59bf1da381016e369
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 13:24:47 -0700

    update 69. Sqrt(x)

--- Sqrt_x.java
+++ Sqrt_x.java
@@ -1,21 +1,21 @@
 public class Solution {
-  public int sqrt(int x) {
-    // Start typing your Java solution below
-    // DO NOT write main() function
-    if( x == 0 || x == 1 )
+  public int mySqrt(int x) {
+    if (x == 0 || x == 1) {
       return x;
+    }
     double left = 0;
     double right = x;
     double epsilon = 0.01;
-    while( true ) {
-      double mid = ( left + right ) / 2;
+    while (true) {
+      double mid = left + (right - left) / 2;
       double sqare = mid * mid;
-      if( Math.abs( sqare - x ) < epsilon )
+      if (Math.abs(sqare - x) < epsilon) {
         return (int) mid;
-      else if( sqare > x )
+      } else if (sqare > x) {
         right = mid;
-      else if( sqare < x )
+      } else if (sqare < x) {
         left = mid;
+      }
     }
   }
 }
