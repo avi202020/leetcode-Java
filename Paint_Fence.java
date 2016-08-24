a147f9d1e6720eb5f9db0854a426a1e727fc9b5b
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 22:23:45 -0700

    update Paint Fence

--- Paint_Fence.java
+++ Paint_Fence.java
@@ -1,3 +1,25 @@
+// Solution 1.
+public class Solution {
+  public int numWays(int n, int k) {
+    if (n == 0 || k == 0) {
+      return 0;
+    }
+    if (n == 1) {
+      return k;
+    }
+    int[] dp = new int[n + 1];
+    dp[1] = k;
+    dp[2]= k + k * (k - 1);
+    for (int i = 3; i <= n; i++) {
+      // (k - 1) * dp[i - 2] means that (i) is in same color as (i-1)
+      // (k - 1) * dp[i - 1] means that (i) is in different color as (i-1)
+      dp[i] = (k - 1) * dp[i - 2] + (k - 1) * dp[i - 1];
+    }
+    return dp[n];
+  }
+}
+
+// Solution 2.
 public class Solution {
   public int numWays(int n, int k) {
     if (n == 0 || k == 0) {
