41abfb99d5829c2ce0a50cc46478d95fc6cf96cf
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 01:32:37 -0700

    udpate 56. Merge Intervals

--- Merge_Intervals.java
+++ Merge_Intervals.java
@@ -7,37 +7,30 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-
 public class Solution {
-	public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<Interval> ans = new ArrayList<Interval>();
-		if( intervals.size() == 0 )
-			return ans;
-		Collections.sort( intervals, new mycmp() );
-		Interval itv = intervals.get(0);
-		for( int i = 1; i < intervals.size(); ++i )
-		{
-			if( intervals.get(i).start > itv.end )
-			{
-				ans.add(itv);
-				itv = intervals.get(i);
-			}
-			else if( intervals.get(i).end > itv.end )
-				itv.end = intervals.get(i).end;
-		}
-		ans.add(itv);
-		return ans;
-	}
-	class mycmp implements Comparator
-	{
-		public int compare( Object o1, Object o2 )
-		{
-			Interval i1 = (Interval)o1;
-			Interval i2 = (Interval)o2;
-			if( i1.start > i2.start ) return 1;
-			else return -1;
-		}
-	}
+  public List<Interval> merge(List<Interval> intervals) {
+    List<Interval> ans = new ArrayList<Interval>();
+    if (intervals == null || intervals.size() == 0) {
+      return ans;
+    }
+    Collections.sort(intervals, new Comparator<Interval>() {
+      public int compare(Interval o1, Interval o2) {
+        return o1.start - o2.start;
+      }
+    });
+    Interval previous = intervals.get(0);
+    for (int i = 1; i < intervals.size(); ++i) {
+      Interval current = intervals.get(i);
+      if (current.start > previous.end) {
+        // case 1. the current interval is completely behind the previous one
+        ans.add(previous);
+        previous = current;
+      } else if (current.end > previous.end) {
+        // case 2. overlap and the current interval has longer tail
+        previous.end = current.end;
+      }
+    }
+    ans.add(previous);
+    return ans;
+  }
 }
