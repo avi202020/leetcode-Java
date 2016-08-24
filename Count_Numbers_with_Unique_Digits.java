1c1ee646a2181042bcb1d9a511264c04dc89776d
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 17:28:25 -0700

    add Count Numbers with Unique Digits

---
+++ Count_Numbers_with_Unique_Digits.java
@@ -0,0 +1,25 @@
+public class Solution {
+  public int countNumbersWithUniqueDigits(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    int res = 0;
+    for (int i = 1; i <= n; ++i) {
+      res += countNumbersWithUniqueDigitsHelper(i);
+    }
+    return res;
+  }
+  private int countNumbersWithUniqueDigitsHelper(int k) {
+    if (k < 1) {
+      return 0;
+    }
+    if (k == 1) {
+      return 10;
+    }
+    int res = 1;
+    for (int i = 9; i >= (11 - k); --i) {
+      res *= i;
+    }
+    return res * 9;
+  }
+}
