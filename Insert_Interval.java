97ac14871f608d1100128a4f667557a1ad70f286
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 01:21:56 -0700

    update Insert Interval

--- Insert_Interval.java
+++ Insert_Interval.java
@@ -8,43 +8,40 @@
  * }
  */
 public class Solution {
-    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        ArrayList<Interval> ans = new ArrayList<Interval>();
-        // true for: the newInterval has been inserted.
-        boolean flag = false;
-        for( int i = 0; i < intervals.size(); ++i ) {
-            // process case 1. current interval is totally "before" newInterval
-            if( intervals.get(i).end < newInterval.start ) {
-                ans.add(intervals.get(i));
-                continue;
-            }
-            // process case 2. current interval is totally "behind" newInterval
-            if( intervals.get(i).start > newInterval.end ) {
-                if( flag == false ) {
-                    ans.add(newInterval);
-                    flag = true;
-                    ans.add(intervals.get(i));
-                }
-                else if( flag == true )
-                    ans.add(intervals.get(i));
-                continue;
-            }
-            // process case 3. overlap
-            if( intervals.get(i).start < newInterval.start ) {
-                newInterval.start = intervals.get(i).start;
-            }
-            if( intervals.get(i).end > newInterval.end ) {
-                newInterval.end = intervals.get(i).end;
-                // Since there is only one interval to be inserted and others are separated.
-                // The insertion is already done here.
-                ans.add(newInterval);
-                flag = true;
-            }
+  public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    List<Interval> ans = new ArrayList<Interval>();
+    // true for: the newInterval has been inserted.
+    boolean flag = false;
+    for (int i = 0; i < intervals.size(); ++i) {
+      // case 1. current interval is totally "before" newInterval
+      if (intervals.get(i).end < newInterval.start) {
+        ans.add(intervals.get(i));
+        continue;
+      }
+      // case 2. current interval is totally "behind" newInterval
+      if (intervals.get(i).start > newInterval.end) {
+        if (!flag) {
+          ans.add(newInterval);
+          flag = true;
         }
-        if( flag == false )
-            ans.add(newInterval);
-        return ans;
+        ans.add(intervals.get(i));
+        continue;
+      }
+      // case 3. overlap
+      if (intervals.get(i).start < newInterval.start) {
+        newInterval.start = intervals.get(i).start;
+      }
+      if (intervals.get(i).end > newInterval.end) {
+        newInterval.end = intervals.get(i).end;
+        // Since there is only one interval to be inserted and others are separated.
+        // The insertion is already done here.
+        ans.add(newInterval);
+        flag = true;
+      }
     }
+    if (!flag) {
+      ans.add(newInterval);
+    }
+    return ans;
+  }
 }
