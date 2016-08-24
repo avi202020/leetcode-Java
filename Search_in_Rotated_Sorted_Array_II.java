f6aa5e8bf18275faa8e0c237b1284afbc99d81c0
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 02:29:58 -0700

    update 81. Search in Rotated Sorted Array II

--- Search_in_Rotated_Sorted_Array_II.java
+++ Search_in_Rotated_Sorted_Array_II.java
@@ -1,34 +1,35 @@
 public class Solution {
-	public boolean search(int[] A, int target) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int left = 0;
-		int right = A.length - 1;
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			if (A[mid] == target)
-				return true;
-			if (A[left] < A[mid]) {
-				if (target >= A[left] && target <= A[mid])
-					right = mid - 1;
-				else
-					left = mid + 1;
-			} else if (A[mid] < A[left]) {
-				if (target <= A[right] && target >= A[mid])
-					left = mid + 1;
-				else
-					right = mid - 1;
-			} else {
-				if (A[left] == target)
-					return true;
-				while (A[left] == A[mid]) {
-					left++;
-					if( left > right )
-						return false;
-				}
-			}
-		}
-		return false;
-	}
-
+  public boolean search(int[] A, int target) {
+    int left = 0;
+    int right = A.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (A[mid] == target) {
+        return true;
+      }
+      if (A[left] < A[mid]) {
+        if (target >= A[left] && target <= A[mid]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else if (A[left] > A[mid]) {
+        if (target <= A[right] && target >= A[mid]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      } else {
+        if (A[left] == target) {
+          return true;
+        }
+        while (A[left] == A[mid]) {
+          left++;
+          if (left > right)
+            return false;
+        }
+      }
+    }
+    return false;
+  }
 }
