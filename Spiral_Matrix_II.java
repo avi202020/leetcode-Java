98dd8ebb060c068329eee3f4e446755bbbd07097
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 23:24:33 -0700

    update Spiral Matrix II

--- Spiral_Matrix_II.java
+++ Spiral_Matrix_II.java
@@ -1,29 +1,28 @@
 public class Solution {
-    int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-    public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
-        if( n == 0 ) return matrix;
-        boolean[][] visit = new boolean[n][n];
-        dfsGenerateMatrix(0, 0, 1, 0, visit, matrix);
-        return matrix;
+  public int[][] generateMatrix(int n) {
+    int[][] matrix = new int[n][n];
+    if (n <= 0) {
+      return matrix;
     }
-
-    public void dfsGenerateMatrix(int x, int y, int cur, int curdir, boolean[][] visit, int[][] matrix) {
-        if (cur > visit.length * visit[0].length)
-            return;
-        matrix[x][y] = cur;
-        visit[x][y] = true;
-        int nx = x + dir[curdir][0];
-        int ny = y + dir[curdir][1];
-        if (nx >= 0 && nx < visit.length && ny >= 0 && ny < visit[0].length && visit[nx][ny] == false) {
-            dfsGenerateMatrix(nx, ny, cur + 1, curdir, visit, matrix);
-        } else {
-            curdir = (curdir + 1) % 4;
-            nx = x + dir[curdir][0];
-            ny = y + dir[curdir][1];
-            dfsGenerateMatrix(nx, ny, cur + 1, curdir, visit, matrix);
-        }
+    boolean[][] visited = new boolean[n][n];
+    int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    generateMatrixHelper(matrix, 0, 0, 1, dirs, 0, visited);
+    return matrix;
+  }
+  private void generateMatrixHelper(int[][] matrix, int x, int y, int depth,
+                                    int[][] dirs, int dir, boolean[][] visited) {
+    if (depth > matrix.length * matrix[0].length) {
+      return;
     }
+    visited[x][y] = true;
+    matrix[x][y] = depth;
+    int nx = x + dirs[dir][0];
+    int ny = y + dirs[dir][1];
+    if (nx < 0 || nx >= matrix.length || ny < 0 || ny >= matrix[0].length || visited[nx][ny]) {
+      dir = (dir + 1) % 4;
+      nx = x + dirs[dir][0];
+      ny = y + dirs[dir][1];
+    }
+    generateMatrixHelper(matrix, nx, ny, depth + 1, dirs, dir, visited);
+  }
 }
-
