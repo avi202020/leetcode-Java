e37a6dd3376a844413af9ecaa312e032926cf492
jintaoguan <ilovexiao77@gmail.com>
2016-08-23 23:42:52 -0700

    add 179. Largest Number

--- Largest_Number.java
+++ Largest_Number.java
@@ -1,20 +1,26 @@
 public class Solution {
-  public String largestNumber(int[] num) {
-    String strs[] = new String[num.length];
-    for( int i = 0; i < strs.length; ++i )
-      strs[i] = String.valueOf(num[i]);
-    Arrays.sort(strs, new MyComparator() );
-    StringBuilder sb = new StringBuilder();
-    for( int i = 0; i < strs.length; ++i )
-      sb.append(strs[i]);
-    java.math.BigInteger result = new java.math.BigInteger(sb.toString());
-    return result.toString();
-  }
-  class MyComparator implements Comparator<String> {
-    public int compare(String a, String b)  {
-      String str1 = a.concat(b);
-      String str2 = b.concat(a);
-      return str2.compareTo(str1);
+  public String largestNumber(int[] nums) {
+    List<Integer> numbers = new ArrayList<Integer>();
+    for (int i = 0; i < nums.length; ++i) {
+      numbers.add(nums[i]);
     }
+    Collections.sort(numbers, new Comparator<Integer>(){
+      public int compare(Integer o1, Integer o2) {
+        String tmp1 = "" + o1 + o2;
+        String tmp2 = "" + o2 + o1;
+        return tmp2.compareTo(tmp1);
+      }
+    });
+    StringBuilder res = new StringBuilder();
+    for (int i = 0; i < numbers.size(); ++i) {
+      if (numbers.get(i) == 0 && res.length() == 0) {
+        continue;
+      }
+      res.append(numbers.get(i));
+    }
+    if (res.length() == 0) {
+      return "0";
+    }
+    return res.toString();
   }
 }
