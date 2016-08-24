21f9dc1116f5f98ad6e184eaa2c7412c1001081c
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 01:07:28 -0700

    update 48. Rotate Image

--- Rotate_Image.java
+++ Rotate_Image.java
@@ -1,16 +1,20 @@
+// Solution 1.
 public class Solution {
-	public void rotate(int[][] matrix) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int n = matrix.length;
-		int[][] result = new int[n][];
-		for( int i = 0; i < n; ++i )
-			result[i] = new int[n];
-		for( int i = 0; i < n; ++i )
-			for( int j = 0; j < n; ++j )
-				result[i][j] = matrix[n-1-j][i];
-		for( int i = 0; i < n; ++i )
-			for( int j = 0; j < n; ++j )
-				matrix[i][j] = result[i][j];
-	}
+  public void rotate(int[][] matrix) {
+    int n = matrix.length;
+    int[][] result = new int[n][];
+    for (int i = 0; i < n; ++i) {
+      result[i] = new int[n];
+    }
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        result[i][j] = matrix[n - 1 - j][i];
+      }
+    }
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        matrix[i][j] = result[i][j];
+      }
+    }
+  }
 }
