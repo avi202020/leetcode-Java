ec9ad226bd4fb50f84b95ca6ebed3bb6ece32bc1
jintaoguan <ilovexiao77@gmail.com>
2016-08-24 00:38:45 -0700

    update Jump Game

--- Jump_Game.java
+++ Jump_Game.java
@@ -1,16 +1,14 @@
 public class Solution {
-	public boolean canJump(int[] A) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int cur = 0;
-		int max = 0;
-		while( cur <= max ) {
-			if( cur + A[cur] > max )
-				max = cur + A[cur];
-			if( max >= A.length - 1 )
-				return true;
-			cur++;
-		}
-		return false;
-	}
+  public boolean canJump(int[] nums) {
+    int max = nums[0];
+    int left = 0;
+    while (left <= max) {
+      max = Math.max(max, left + nums[left]);
+      if (max >= nums.length - 1) {
+        return true;
+      }
+      left++;
+    }
+    return false;
+  }
 }
