461b4ac59b7fbf47ec88e29cbc94833132efe057
jintaoguan <ilovexiao77@gmail.com>
2016-08-25 12:52:33 -0700

    update 35. Search Insert Position

--- Search_Insert_Position.java
+++ Search_Insert_Position.java
@@ -1,24 +1,24 @@
 public class Solution {
-	public int searchInsert(int[] A, int target) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( target <= A[0] ) 
-			return 0;
-		if( target > A[A.length-1] ) 
-			return A.length;
-		int left = 0;
-		int right = A.length - 1;
-		int mid = 0;
-		while( left <= right )
-		{
-			mid = ( left + right ) / 2;
-			if( target <= A[mid] && target > A[mid-1] )
-				return mid;
-			else if( A[mid] < target )
-				left = mid + 1;
-			else if( A[mid] >= target )
-				right = mid - 1;
-		}
-		return mid;
-	}
+  public int searchInsert(int[] nums, int target) {
+    if (target <= nums[0]) {
+      return 0;
+    }
+    if (target > nums[nums.length - 1]) {
+      return nums.length;
+    }
+    int left = 0;
+    int right = nums.length - 1;
+    int mid = 0;
+    while (left <= right) {
+      mid = left + (right - left) / 2;
+      if (target <= nums[mid] && target > nums[mid-1]) {
+        return mid;
+      } else if (target > nums[mid]) {
+        left = mid + 1;
+      } else if (target <= nums[mid]) {
+        right = mid - 1;
+      }
+    }
+    return mid;
+  }
 }
