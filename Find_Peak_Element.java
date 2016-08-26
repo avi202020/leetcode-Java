294363e2a0d72852185e2fa3cf21ee1d120113aa
jintaoguan <ilovexiao77@gmail.com>
2016-08-26 16:53:32 -0700

    add 162. Find Peak Element

--- Find_Peak_Element.java
+++ Find_Peak_Element.java
@@ -1,3 +1,22 @@
+// Solution 1. binary search
+public class Solution {
+  public int findPeakElement(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] >= nums[mid + 1]) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return left;
+  }
+}
+
+
+// Solution 2. linear search
 public class Solution {
   public int findPeakElement(int[] num) {
     if( num.length == 1 )
