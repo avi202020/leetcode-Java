92f9169f8bee99d5fcf0b06cc40a17dfc91d64e0
jintaoguan <ilovexiao77@gmail.com>
2016-08-26 19:10:22 -0700

    update 91. Decode Ways

--- Decode_Ways.java
+++ Decode_Ways.java
@@ -1,22 +1,31 @@
 public class Solution {
-	public int numDecodings(String s) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if( s.length() == 0 ) return 0;
-		if( s.charAt(0) == '0' ) return 0;
-		int[] dp = new int[s.length()+1];
-		dp[0] = 1;
-		dp[1] = s.charAt(0) == '0' ? 0 : 1; 
-		for( int i = 2; i <= s.length(); ++i )
-		{
-			int tmp = 0;
-			if( s.charAt(i-1) != '0' )
-				tmp += dp[i-1];
-			if( (s.charAt(i-2)-'0')*10 + (s.charAt(i-1)-'0') >= 10 
-					&& (s.charAt(i-2)-'0')*10 + (s.charAt(i-1)-'0') <= 26 )
-				tmp += dp[i-2];
-			dp[i] = tmp;
-		}
-		return dp[s.length()];
-	} 
+  public int numDecodings(String s) {
+    if (s == null || s.length() == 0 || s.startsWith("0")) {
+      return 0;
+    }
+    if (s.length() == 1) {
+      if (s.equals("0")) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+    int[] dp = new int[s.length() + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= s.length(); ++i) {
+      char cur = s.charAt(i-1);
+      char pre = s.charAt(i-2);
+      int one = (cur - '0');
+      int two = (pre - '0') * 10 + one;
+      dp[i] = 0;
+      if (two >= 10 && two <= 26) {
+        dp[i] += dp[i-2];
+      }
+      if (one != 0) {
+        dp[i] += dp[i-1];
+      }
+    }
+    return dp[s.length()];
+  }
 }
