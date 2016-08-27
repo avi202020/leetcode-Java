3685e0520b15fa678ec0d0e40a4c29777221fb86
jintaoguan <ilovexiao77@gmail.com>
2016-08-27 00:07:22 -0700

    update 73. Set Matrix Zeroes

--- Set_Matrix_Zeroes.java
+++ Set_Matrix_Zeroes.java
@@ -1,21 +1,71 @@
+// Solution 1. O(1) space
 public class Solution {
-	public void setZeroes(int[][] matrix) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int m = matrix.length;
-		int n = matrix[0].length;
-		boolean[] row = new boolean[m];
-		boolean[] col = new boolean[n];
-		for( int i = 0; i < m; ++i )
-			for( int j = 0; j < n; ++j )
-				if( matrix[i][j] == 0 )
-				{
-					row[i] = true;
-					col[j] = true;
-				}
-		for( int i = 0; i < m; ++i )
-			for( int j = 0; j < n; ++j )
-				if( row[i] || col[j] )
-					matrix[i][j] = 0;
-	}
+  public void setZeroes(int[][] matrix) {
+    if (matrix == null || matrix.length <= 0) {
+      return;
+    }
+    boolean row0has0 = false;
+    boolean col0has0 = false;
+    for (int i = 0; i < matrix.length; i++) {
+      if (matrix[i][0] == 0) {
+        col0has0 = true;
+      }
+    }
+    for (int j = 0; j < matrix[0].length; j++) {
+      if (matrix[0][j] == 0) {
+        row0has0 = true;
+      }
+    }
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[0].length; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[0].length; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+    if (row0has0) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+    if (col0has0) {
+      for (int i = 0; i < matrix.length; i++) {
+        matrix[i][0] = 0;
+      }
+    }
+  }
+}
+
+
+// Solution 2. native solution
+public class Solution {
+  public void setZeroes(int[][] matrix) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    boolean[] row = new boolean[m];
+    boolean[] col = new boolean[n];
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (matrix[i][j] == 0) {
+          row[i] = true;
+          col[j] = true;
+        }
+      }
+    }
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (row[i] || col[j]) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
 }
