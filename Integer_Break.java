9c06a9ded79bc390afc13f62a641b8108ae0a62b
jintaoguan <ilovexiao77@gmail.com>
2016-08-26 18:31:36 -0700

    update 343. Integer Break

--- Integer_Break.java
+++ Integer_Break.java
@@ -1,3 +1,4 @@
+// Solution 1. 
 public class Solution {
   public int integerBreak(int n) {
     if (n == 2) {
@@ -10,16 +11,34 @@ public class Solution {
       return 4;
     }
     int result = 1;
-    if (n % 3 == 0) {
-      int m = n / 3;
-      result = (int) Math.pow(3, m);
-    } else if (n % 3 == 2) {
-      int m = n / 3;
-      result = (int) Math.pow(3, m) * 2;
-    } else if (n % 3 == 1) {
-      int m = (n - 4) / 3;
-      result = (int) Math.pow(3, m) * 4;
+    while (n > 4) {
+      result *= 3;
+      n -= 3;
     }
+    result *= n;
     return result;
+  }
+}
+
+// Solution 2. DP
+public class Solution {
+  public int integerBreak(int n) {
+    if (n == 2) {
+      return 1;
+    }
+    if (n == 3) {
+      return 2;
+    }
+    if (n == 4) {
+      return 4;
+    }
+    int[] dp = new int[n+1];
+    dp[2] = 2;
+    dp[3] = 3;
+    dp[4] = 4;
+    for (int i = 5; i <= n; ++i) {
+      dp[i] = dp[i - 3] * 3;
+    }
+    return dp[n];
   }
 }
