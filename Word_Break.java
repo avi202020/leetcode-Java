d7077a88c5d0f0dff1c627e61c58402989649b68
jintaoguan <ilovexiao77@gmail.com>
2016-08-30 11:44:42 -0700

    update 139. Word Break

--- Word_Break.java
+++ Word_Break.java
@@ -1,40 +1,43 @@
+// Solution 1. DP 
 public class Solution {
-	boolean success;
-	public boolean wordBreak(String s, Set<String> dict) {
-		// IMPORTANT: Please reset any member data you declared, as
-		// the same Solution instance will be reused for each test case.
-		success = false;
+  public boolean wordBreak(String s, Set<String> wordDict) {
+    boolean[] dp = new boolean[s.length()];
+    for (int i = 0; i < s.length(); ++i) {
+      if (wordDict.contains(s.substring(0, i + 1))) {
+        dp[i] = true;
+      } else {
+        dp[i] = false;
+        for (int j = 0; j < i; ++j) {
+          if (dp[j] && wordDict.contains(s.substring(j + 1, i + 1))) {
+            dp[i] = true;
+            break;
+          }
+        }
+      }
+    }
+    return dp[s.length() - 1];
+  }
+} 
 
-		// optimization: 
-		// if there is a char of s that doesn't exist in dict, return false
-		HashSet<Character> present = new HashSet<Character>();
-		Iterator<String> it = dict.iterator();
-		while(it.hasNext()) {
-			String str = it.next();
-			for( int i = 0; i < str.length(); ++i )
-				present.add( str.charAt(i) );
-		}
-		for( int i = 0; i < s.length(); ++i ) {
-			if( !present.contains(s.charAt(i)) )
-				return false;
-		}
-		dfs( s, dict, 0 );
-		return success;
-	}
-	public boolean dfs( String s, Set<String> dict, int depth ) {
-		if( depth >= s.length() ) {
-			success = true;
-			return true;
-		}
-		if( success == true )
-			return true;
-		for( int i = depth + 1; i <= s.length(); ++i ) {
-			if( dict.contains( s.substring(depth,i) ) ) {
-				boolean found = dfs( s, dict, i );
-				if( found == true )
-					return true;
-			}
-		}
-		return false;
-	}
-}
+
+// Solution 2. DFS but got TLE
+public class Solution {
+  public boolean wordBreak(String s, Set<String> wordDict) {
+    return wordBreakHelper(s, wordDict, 0);
+  }
+  private boolean wordBreakHelper(String s, Set<String> wordDict, int depth) {
+    if (depth >= s.length()) {
+      return true;
+    }
+    for (int i = depth + 1; i <= s.length(); ++i) {
+      String prefix = s.substring(depth, i);
+      if (wordDict.contains(prefix)) {
+        boolean success = wordBreakHelper(s, wordDict, i);
+        if (success) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+} 
