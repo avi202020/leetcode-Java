c4820db914137af92acdbd2c1b6c190d69ea7ddc
jintaoguan <ilovexiao77@gmail.com>
2016-08-30 16:27:45 -0700

    add 239. Sliding Window Maximum

---
+++ Sliding_Window_Maximum.java
@@ -0,0 +1,27 @@
+// Solution 1. naive solution using heap O(nlogn) time
+public class Solution {
+  public int[] maxSlidingWindow(int[] nums, int k) {
+    if (k == 0) {
+      return new int[0];
+    }
+    int[] res = new int[nums.length - k + 1];
+    int index = 0;
+    PriorityQueue<Integer> queue = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+      }
+    });
+    for (int i = 0; i < nums.length; ++i) {
+      queue.add(nums[i]);
+      if (queue.size() > k) {
+        queue.remove(nums[i - k]);
+      }
+      if (queue.size() == k) {
+        res[index] = queue.peek();
+        index++;
+      }
+    }
+    return res;
+  }
+}
