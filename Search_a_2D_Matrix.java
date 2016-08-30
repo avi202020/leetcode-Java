d4f689c3276bd48969e6e129c59bfcf95fa59d44
jintaoguan <ilovexiao77@gmail.com>
2016-08-30 16:49:50 -0700

    update 74. Search a 2D Matrix

--- Search_a_2D_Matrix.java
+++ Search_a_2D_Matrix.java
@@ -1,20 +1,24 @@
 // Solution 1.
 public class Solution {
   public boolean searchMatrix(int[][] matrix, int target) {
-    // Start typing your Java solution below
-    // DO NOT write main() function
-    if( matrix.length == 0 ) return false;
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+      return false;
+    }
     int m = matrix.length;
     int n = matrix[0].length;
     int x = 0;
     int y = n - 1;
-    while( x < m && y >= 0 ) {
-      if( matrix[x][y] > target )
-        y--;
-      else if( matrix[x][y] < target )
-        x++;
-      else if( matrix[x][y] == target )
+    while (true) {
+      if (x < 0 || x >= m || y < 0 || y >= n) {
+        break;
+      }
+      if (matrix[x][y] == target) {
         return true;
+      } else if (matrix[x][y] < target) {
+        x++;
+      } else {
+        y--;
+      }
     }
     return false;
   }
@@ -22,27 +26,27 @@ public class Solution {
 
 
 
-// Solution 2. totally a Binary Search
+// Solution 2. totally a Binary Search, because "The first integer of each row is greater than the last integer of the previous row."
 public class Solution {
   public boolean searchMatrix(int[][] matrix, int target) {
-    // IMPORTANT: Please reset any member data you declared, as
-    // the same Solution instance will be reused for each test case.
-    if( matrix.length == 0 )
+    if (matrix.length == 0) {
       return false;
+    }
     int m = matrix.length;
     int n = matrix[0].length;
     int left = 0;
     int right = m * n - 1;
-    while( left <= right ) {
-      int mid = ( left + right ) / 2;
+    while (left <= right) {
+      int mid = (left + right) / 2;
       int row = mid / n;
       int col = mid % n;
-      if( target > matrix[row][col] )
+      if (target > matrix[row][col]) {
         left = mid + 1;
-      else if( target < matrix[row][col] )
+      } else if (target < matrix[row][col]) {
         right = mid - 1;
-      else if( target == matrix[row][col] )
+      } else if (target == matrix[row][col]) {
         return true;
+      }
     }
     return false;
   }
