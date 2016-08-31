279533cad9f3fa6660d08b3f1dfe0816a6cedd24
jintaoguan <ilovexiao77@gmail.com>
2016-08-31 14:07:05 -0700

    add 250. Count Univalue Subtrees

---
+++ Count_Univalue_Subtrees.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+  public int countUnivalSubtrees(TreeNode root) {
+    int[] count=new int[1];
+    countUnivalSubtreesHelper(root,count);
+    return count[0];
+  }
+  private boolean countUnivalSubtreesHelper(TreeNode root, int[] count) {
+    if (root == null) {
+      return true;
+    }
+    boolean left = countUnivalSubtreesHelper(root.left, count);
+    boolean right = countUnivalSubtreesHelper(root.right, count);
+    if (!left || !right) {
+      return false;
+    }
+    if (root.left != null && root.val != root.left.val) {
+      return false;
+    }
+    if (root.right != null && root.val != root.right.val) {
+      return false;
+    }
+    count[0]++;
+    return true;
+  }
+}
