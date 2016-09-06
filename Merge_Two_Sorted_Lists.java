b438c421fc0b90085aeefb57be387e02b39a9798
jintaoguan <ilovexiao77@gmail.com>
2016-09-06 12:31:15 -0700

    update 21. Merge Two Sorted Lists

--- Merge_Two_Sorted_Lists.java
+++ Merge_Two_Sorted_Lists.java
@@ -12,11 +12,15 @@
 
 public class Solution {
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    // Start typing your Java solution below
-    // DO NOT write main() function
-    if (l1 == null && l2 == null) return null;
-    if (l1 == null) return l2;
-    if (l2 == null) return l1;
+    if (l1 == null && l2 == null) {
+      return null;
+    }
+    if (l1 == null) {
+      return l2;
+    }
+    if (l2 == null) {
+      return l1;
+    }
     ListNode ans = l1.val < l2.val ? l1 : l2;
     ListNode pre = ans;
     if (l1.val < l2.val) l1 = l1.next;
