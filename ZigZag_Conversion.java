e59194f9003a570a7a35d8f47a60e1c783cea195
jintaoguan <ilovexiao77@gmail.com>
2016-09-06 01:15:50 -0700

    update 6. ZigZag Conversion

--- ZigZag_Conversion.java
+++ ZigZag_Conversion.java
@@ -1,23 +1,28 @@
 public class Solution {
   public String convert(String s, int nRows) {
-    if( nRows == 0 )
+    if (nRows == 0) {
       return "";
-    if( nRows == 1 )
+    }
+    if (nRows == 1) {
       return s;
+    }
     ArrayList<StringBuilder> arr = new ArrayList<StringBuilder>();
-    for( int i = 0; i < nRows; ++i )
+    for (int i = 0; i < nRows; ++i) {
       arr.add(new StringBuilder());
+    }
     int dir = 1;
     int cur = 0;
-    for( int i = 0; i < s.length(); ++i ) {
+    for (int i = 0; i < s.length(); ++i) {
       arr.get(cur).append(s.charAt(i));
       cur = cur + dir;
-      if( cur == 0 || cur == nRows - 1 )
+      if (cur == 0 || cur == nRows - 1) {
         dir = dir * (-1);
+      }
     }
     StringBuilder ans = new StringBuilder();
-    for( int i = 0; i < nRows; ++i )
+    for (int i = 0; i < nRows; ++i) {
       ans.append(arr.get(i).toString());
+    }
     return ans.toString();
   }
 }
