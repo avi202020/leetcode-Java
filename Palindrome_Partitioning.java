127eb745e8d2d5578328f1179f45d6bbd435db5e
jintaoguan <ilovexiao77@gmail.com>
2016-09-08 13:44:42 -0700

    update 131. Palindrome Partitioning

--- Palindrome_Partitioning.java
+++ Palindrome_Partitioning.java
@@ -1,32 +1,34 @@
 public class Solution {
   public List<List<String>> partition(String s) {
-    ArrayList<List<String>> ans = new ArrayList<List<String>>();
-    if( s == null || s.length() == 0 )
+    List<List<String>> ans = new ArrayList<List<String>>();
+    if (s == null || s.length() == 0) {
       return ans;
-    ArrayList<String> path = new ArrayList<String>();
-    _partitionHelper(ans, path, s, 0);
+    }
+    List<String> path = new ArrayList<String>();
+    partitionHelper(s, 0, path, ans);
     return ans;
   }
-  public void _partitionHelper(List<List<String>> ans, List<String> path, String s, int depth ) {
-    if( depth >= s.length() ) {
+  public void partitionHelper(String s, int depth, List<String> path, List<List<String>> ans) {
+    if (depth >= s.length()) {
       ArrayList<String> tmp = new ArrayList<String>(path);
       ans.add(tmp);
       return;
     }
-    for( int i = depth; i > s.length(); ++i ) {
-      if( _isPalindrome(s.substring(depth, i + 1)) ) {
-        path.add(s.substring(depth, i+1));
-        _partitionHelper(ans, path, s, i + 1);
+    for (int i = depth + 1; i <= s.length(); ++i) {
+      if (isPalindromeHelper(s.substring(depth, i))) {
+        path.add(s.substring(depth, i));
+        partitionHelper(s, i, path, ans);
         path.remove(path.size() - 1);
       }
     }
   }
-  public boolean _isPalindrome(String s) {
+  public boolean isPalindromeHelper(String s) {
     int left = 0;
     int right = s.length() - 1;
-    while( left < right ) {
-      if( s.charAt(left) != s.charAt(right) )
+    while (left < right) {
+      if (s.charAt(left) != s.charAt(right)) {
         return false;
+      }
       left++;
       right--;
     }
