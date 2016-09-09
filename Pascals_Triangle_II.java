10251d4dda3cd8a805b4ac09963282751a187a05
jintaoguan <ilovexiao77@gmail.com>
2016-09-09 02:39:44 -0700

    update 119. Pascal's Triangle II

--- Pascals_Triangle_II.java
+++ Pascals_Triangle_II.java
@@ -1,21 +1,19 @@
 public class Solution {
-	public ArrayList<Integer> getRow(int rowIndex) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ArrayList<Integer> ans = new ArrayList<Integer>();
-		for( int i = 0; i <= rowIndex; ++i )
-			ans.add(0);
-		ans.set( 0, 1 );
-		for( int i = 1; i <= rowIndex; ++i )
-		{
-			for( int j = i; j >= 0; --j )
-			{
-				if( j == i || j == 0 ) 
-					ans.set( j, 1 );
-				else
-					ans.set( j, ans.get(j-1) + ans.get(j) );
-			}
-		}
-		return ans;
-	}
+  public List<Integer> getRow(int rowIndex) {
+    List<Integer> ans = new ArrayList<Integer>();
+    for (int i = 0; i <= rowIndex; ++i) {
+      ans.add(0);
+    }
+    ans.set(0, 1);
+    for (int i = 1; i <= rowIndex; ++i) {
+      for (int j = i; j >= 0; --j) {
+        if (j == i || j == 0) {
+          ans.set(j, 1);
+        } else {
+          ans.set(j, ans.get(j - 1) + ans.get(j));
+        }
+      }
+    }
+    return ans;
+  }
 }
