030c77a1aaa392eae88d2b2f6ae32da8b8a5b264
jintaoguan <ilovexiao77@gmail.com>
2016-09-12 01:05:26 -0700

    update 78. Subsets

--- Subsets.java
+++ Subsets.java
@@ -1,3 +1,33 @@
+// solution 1.
+public class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> rst = new ArrayList<List<Integer>>();
+    if (nums == null || nums.length == 0) {
+      return rst;
+    }
+    Arrays.sort(nums);
+    List<Integer> list = new ArrayList<Integer>();
+    subsetsHelper(rst, list, nums, 0);
+    return rst;
+  }
+
+  public void subsetsHelper(List<List<Integer>> rst, List<Integer> list, int[] nums, int pos) {
+    List<Integer> tmp = new ArrayList<Integer>();
+    for (int num : list) {
+      tmp.add(num);
+    }
+    rst.add(tmp);
+
+    for (int i = pos; i < nums.length; i++) {
+      list.add(nums[i]);
+      subsetsHelper(rst, list, nums, i + 1);
+      list.remove(list.size() - 1);
+    }
+  }
+}
+
+
+// solution 2.
 public class Solution {
   public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> res = new ArrayList<List<Integer>>();
