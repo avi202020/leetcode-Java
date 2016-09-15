77a42fc43c9845b51afbd2109134b8649c06d42d
jintaoguan <ilovexiao77@gmail.com>
2016-09-15 12:49:05 -0700

    update 67. Add Binary

--- Add_Binary.java
+++ Add_Binary.java
@@ -1,23 +1,35 @@
 public class Solution {
-    public String addBinary(String a, String b) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        StringBuffer sb = new StringBuffer();
-        int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        while( i >= 0 || j >= 0 || carry != 0 )
-        {
-            int sum = 0;
-            int value_a = ( i >= 0 ? a.charAt(i) - '0' : 0 );
-            int value_b = ( j >= 0 ? b.charAt(j) - '0' : 0 );
-            sum += value_a + value_b + carry;
-            carry = sum / 2;
-            sum = sum % 2;
-            sb.insert( 0, (char)(sum + '0') );
-            i = (i >= 0 ? --i : i);
-            j = (j >= 0 ? --j : j);
-        }
-        return sb.toString();
+  public String addBinary(String a, String b) {
+    int len1 = a.length();
+    int len2 = b.length();
+    int diff = Math.abs(len2 - len1);
+    if (len1 < len2) {
+      for (int i = 0; i < diff; ++i) {
+        a = "0" + a;
+      }
+    } else if (len1 > len2) {
+      for (int i = 0; i < diff; ++i) {
+        b = "0" + b;
+      }
     }
+    StringBuffer ans = new StringBuffer();
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    while (i >= 0 || j >= 0) {
+      int sum = 0;
+      int digitA = a.charAt(i) - '0';
+      int digitB = b.charAt(j) - '0';
+      sum = digitA + digitB + carry;
+      carry = sum / 2;
+      sum = sum % 2;
+      ans.insert(0, (char) (sum + '0'));
+      i--;
+      j--;
+    }
+    if (carry != 0) {
+      ans.insert(0, "1");
+    }
+    return ans.toString();
+  }
 }
