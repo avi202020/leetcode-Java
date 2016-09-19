a8bc84042ef11b165062134d347b9b7deded0f5e
jintaoguan <ilovexiao77@gmail.com>
2016-09-19 01:49:51 -0700

    update 147. Insertion Sort List

--- Insertion_Sort_List.java
+++ Insertion_Sort_List.java
@@ -3,34 +3,31 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode(int x) { val = x; }
  * }
  */
 public class Solution {
-    public ListNode insertionSortList(ListNode head) {
-        if( head == null || head.next == null )
-            return head;
-
-        ListNode dummy = new ListNode(Integer.MIN_VALUE);
-        ListNode cur = head;
-        while( cur != null ) {
-            ListNode next = cur.next;
-            ListNode prev = findPosition(dummy, cur);
-            cur.next = prev.next;
-            prev.next = cur;
-            cur = next;
-        }
-        return dummy.next;
+  public ListNode insertionSortList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
-
-    public ListNode findPosition(ListNode dummy, ListNode node ) {
-        ListNode cur = dummy;
-        while( cur.next != null && cur.next.val <= node.val ) {
-            cur = cur.next;
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = head;
+    dummyHead.next = head;
+    while (p.next != null) {
+      if (p.val <= p.next.val) {
+        p = p.next;
+      } else {
+        ListNode temp = p.next;
+        ListNode q = dummyHead;
+        p.next = p.next.next;
+        while (q.next.val < temp.val) {  
+          q = q.next;
         }
-        return cur;
+        temp.next = q.next;  
+        q.next = temp;
+      }
     }
+    return dummyHead.next;
+  }
 }
