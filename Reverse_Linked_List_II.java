d2d5bbc052ee0aee84e6fc9d69837cd9cef7f3d2
jintaoguan <ilovexiao77@gmail.com>
2016-09-19 12:00:07 -0700

    update 92. Reverse Linked List II

--- Reverse_Linked_List_II.java
+++ Reverse_Linked_List_II.java
@@ -3,48 +3,44 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode(int x) { val = x; }
  * }
  */
 public class Solution {
-	public ListNode reverseBetween(ListNode head, int m, int n) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		ListNode pre = null;
-		ListNode post = null;
-		ListNode tail = null;
-		int dist = 0;
-		ListNode tmphead = new ListNode(0);
-		tmphead.next = head;
-		ListNode cur = tmphead;
-		while( cur != null ) {
-			if( dist == m - 1 )
-				pre = cur;
-			if( dist == n  )
-				tail = cur;
-			dist++;
-			cur = cur.next;
-		}
-		post = tail.next;
-		tail.next = null;
-		ListNode newhead = _reverseLinklist( pre.next );
-		pre.next = newhead;
-		cur = newhead;
-		while( cur.next != null )
-			cur = cur.next;
-		cur.next = post;
-		return tmphead.next;
-	}
-
-	public ListNode _reverseLinklist(ListNode head) {
-		if( head.next == null ) return head;
-		ListNode next = head.next;
-		ListNode newhead = _reverseLinklist( head.next );
-		head.next = null;
-		next.next = head;
-		return newhead;
-	}
+  public ListNode reverseBetween(ListNode head, int m, int n) {
+    ListNode pre = null;
+    ListNode post = null;
+    ListNode tail = null;
+    int dist = 0;
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+    ListNode cur = dummy;
+    while (cur != null) {
+      if (dist == m - 1) {
+        pre = cur;
+      }
+      if (dist == n) {
+        tail = cur;
+      }
+      dist++;
+      cur = cur.next;
+    }
+    ListNode mthNode = pre.next;
+    post = tail.next;
+    tail.next = null;
+    ListNode newhead = reverseBetweenHelper(pre.next);
+    pre.next = newhead;
+    mthNode.next = post;
+    return dummy.next;
+  }
+  private ListNode reverseBetweenHelper(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode next = head.next;
+    ListNode newHead = reverseBetweenHelper(head.next);
+    next.next = head;
+    head.next = null;
+    return newHead;
+  }
 }
