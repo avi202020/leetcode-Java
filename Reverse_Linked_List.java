6e03bc01cb5d11e87ab615a8164b0c27bfd34025
jintaoguan <ilovexiao77@gmail.com>
2016-09-19 11:57:10 -0700

    update 206. Reverse Linked List

--- Reverse_Linked_List.java
+++ Reverse_Linked_List.java
@@ -9,14 +9,43 @@
  *     }
  * }
  */
+// solution 1. stack
 public class Solution {
-  public ListNode reverseLinkedList( ListNode head ) {
-   if( head == null ) return null;
-   if( head.next == null ) return head;
-   ListNode next = head.next;
-   ListNode newhead = reverseLinkedList(next);
-   head.next = null;
-   next.next = head;
-   return newhead;
- }
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    Stack<ListNode> stack = new Stack<ListNode>();
+    while (head != null) {
+      stack.push(head);
+      head = head.next;
+    }
+    ListNode newHead = new ListNode(-1);
+    ListNode pre = newHead;
+    while (!stack.isEmpty()) {
+      ListNode node = stack.pop();
+      pre.next = node;
+      pre = node;
+    }
+    pre.next = null;
+    return newHead.next;
+  }
 }
+
+// solution 2. recursion
+public class Solution {
+  public ListNode reverseList(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+    if (head.next == null) {
+      return head;
+    }
+    ListNode next = head.next;
+    ListNode newhead = reverseList(next);
+    next.next = head;
+    head.next = null;
+    return newhead;
+  }
+}
+
