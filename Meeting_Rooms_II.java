20e9100cbc5f893d1cc5d84282b9bc7337cc826f
jintaoguan <ilovexiao77@gmail.com>
2016-09-29 09:19:28 -0700

    add Meeting Rooms II

---
+++ Meeting_Rooms_II.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+  public int minMeetingRooms(Interval[] intervals) {
+    if (intervals == null || intervals.length == 0)
+      return 0;
+    // Sort the intervals by start time
+    Arrays.sort(intervals, new Comparator<Interval>() {
+      public int compare(Interval a, Interval b) {
+        return a.start - b.start;
+      }
+    });
+
+    // Use a min heap to track the minimum end time of merged intervals
+    PriorityQueue<Interval> heap = new PriorityQueue<Interval>(intervals.length, new Comparator<Interval>() {
+      public int compare(Interval a, Interval b) {
+        return a.end - b.end;
+      }
+    });
+
+    // start with the first meeting, put it to a meeting room
+    heap.offer(intervals[0]);
+
+    for (int i = 1; i < intervals.length; i++) {
+      // get the meeting room that finishes earliest
+      Interval interval = heap.poll();
+
+      if (interval.end <= intervals[i].start) {
+        // if the current meeting starts right after
+        // there's no need for a new room, merge the interval
+        interval.end = intervals[i].end;
+      } else {
+        // otherwise, this meeting needs a new room
+        heap.offer(intervals[i]);
+      }
+      heap.offer(interval);
+    }
+
+    return heap.size();
+  }
+}
